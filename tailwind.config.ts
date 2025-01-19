a08a6ad5b6558d1fcdd41c5b295ed8c05a73a334
The-LukeZ <luke.hent3005@gmail.com>
2025-01-19 01:27:33 +0100

    Update Tailwind CSS and DaisyUI configurations for improved compatibility and functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,39 @@
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 import daisyui from 'daisyui';
 
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+export default defineConfig({
 
-  theme: {
-    colors: {
-      'dodger-blue': {
-        '50': '#edfcff',
-        '100': '#d6f6ff',
-        '200': '#b5f1ff',
-        '300': '#83eaff',
-        '400': '#48dbff',
-        '500': '#1ec0ff',
-        '600': '#06a4ff',
-        '700': '#0091ff',
-        '800': '#086ec5',
-        '900': '#0d5e9b',
-        '950': '#0e385d',
-      },
-    },
-    fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {},
-  },
+  // theme: {
+  //   colors: {
+  //     'dodger-blue': {
+  //       '50': '#edfcff',
+  //       '100': '#d6f6ff',
+  //       '200': '#b5f1ff',
+  //       '300': '#83eaff',
+  //       '400': '#48dbff',
+  //       '500': '#1ec0ff',
+  //       '600': '#06a4ff',
+  //       '700': '#0091ff',
+  //       '800': '#086ec5',
+  //       '900': '#0d5e9b',
+  //       '950': '#0e385d',
+  //     },
+  //   },
+  //   fontFamily: {
+  //     sans: ['Poppins', 'sans-serif'],
+  //     serif: ['Merriweather', 'serif'],
+  //   },
+  //   extend: {},
+  // },
 
-  plugins: [typography, forms, containerQueries, daisyui],
+  plugins: [tailwindcss(), typography, forms, containerQueries, daisyui],
 
-  daisyui: {
-    styled: true,
-    themes: ['night'],
-    logs: false,
-  },
-} satisfies Config;
+  appType: "custom",
+  clearScreen: true,
+});
