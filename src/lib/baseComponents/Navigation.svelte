9b5ca38d95b6dda706212a3536e7f0927c080c51
The-LukeZ <luke.hent3005@gmail.com>
2025-01-20 10:54:13 +0100

    Add Navigation component and integrate it into the main page

---
+++ src/lib/baseComponents/Navigation.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<div class="bg-base-200 absolute top-0 right-0 left-0 h-[50px]"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+  import Navigation from '$lib/baseCompomponents/Navigation.svelte';
+</script>
+
+<Navigation />
+
 <h1 class="font-sans">Welcome to SvelteKit</h1>
 <p class="">
   Visit <a href="https://svelte.dev/docs/kit" class="link-primary">svelte.dev/docs/kit</a> to read the documentation
