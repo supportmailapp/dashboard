abb2ed96e4cfcaaabf4c420721fa62e69aad123e
The-LukeZ <luke.hent3005@gmail.com>
2025-01-21 21:39:58 +0100

    refactor: Remove TypeScript from layout script and adjust heading in page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<script>
+<script lang="ts">
   import Navigation from '$lib/baseComponents/Navigation.svelte';
 </script>
 
 <Navigation />
 
-<h1 class="font-sans">Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit</h1>
 <p class="btn">
   Visit <a href="https://svelte.dev/docs/kit" class="link-primary">svelte.dev/docs/kit</a> to read the documentation
 </p>
