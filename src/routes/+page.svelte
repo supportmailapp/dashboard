0c30a12ffd08ff0bf4edf5e1951570593314e495
The-LukeZ <luke.hent3005@gmail.com>
2025-01-21 21:05:53 +0100

    Add navigation component with button and fix import path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Navigation from '$lib/baseCompomponents/Navigation.svelte';
+  import Navigation from '$lib/baseComponents/Navigation.svelte';
 </script>
 
 <Navigation />
 
 <h1 class="font-sans">Welcome to SvelteKit</h1>
-<p class="">
+<p class="btn">
   Visit <a href="https://svelte.dev/docs/kit" class="link-primary">svelte.dev/docs/kit</a> to read the documentation
 </p>
