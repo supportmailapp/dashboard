e4dacbd9cd4f7f5ac078852086fa8bca6846e687
The-LukeZ <luke.hent3005@gmail.com>
2025-01-23 23:18:19 +0100

    feat: add utility functions for transforming Discord API user and guild data

---
+++ src/lib/utils/formatting.ts
@@ -0,0 +1,44 @@
+import type { APIGuildChannel, APIPartialGuild, APIRole, APIUser, GuildChannelType } from "discord-api-types/v10";
+
+export function apiUserToCurrentUser(apiUser: APIUser): Utils.CurrentUser {
+  return {
+    id: apiUser.id,
+    username: apiUser.username,
+    displayName: apiUser.username,
+    avatarHash: apiUser.avatar || null,
+  };
+}
+
+export function apiPartialGuildToPartialGuild(apiGuild: APIPartialGuild): Utils.PartialGuild {
+  return {
+    id: apiGuild.id,
+    name: apiGuild.name,
+    iconHash: apiGuild.icon || null,
+  };
+}
+
+export function apiPartialGuildToCurrentGuild(
+  apiGuild: APIPartialGuild | Utils.PartialGuild,
+  channels: APIGuildChannel<GuildChannelType>[],
+  roles: APIRole[],
+): Utils.CurrentGuild {
+  const iconHash = "icon" in apiGuild ? apiGuild.icon : apiGuild.iconHash || null;
+  return {
+    id: apiGuild.id,
+    name: apiGuild.name,
+    iconHash: iconHash,
+    // TODO: Implement sorting after position, if same position, sort by ID, return new array with adjusted positions
+    channels: channels.map((channel) => ({
+      id: channel.id,
+      name: channel.name || "unknown",
+      type: channel.type,
+      position: channel.position,
+    })),
+    roles: roles.map((role) => ({
+      id: role.id,
+      name: role.name,
+      color: role.color,
+      position: role.position,
+    })),
+  };
+}
