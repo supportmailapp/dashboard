1accdd69d49726e960fc2119d2f3c1a638909592
The-LukeZ <luke.hent3005@gmail.com>
2025-01-23 23:17:33 +0100

    feat: migrate hooks.server.js to hooks.server.ts with enhanced authentication and guild handling

---
+++ src/hooks.server.ts
@@ -0,0 +1,80 @@
+import { getUserData, getUserGuilds } from "$lib/discord/oauth2";
+import { decodeToken, verifyTokenPayload, type CookieToken, type FullCookieToken } from "$lib/server/auth";
+import { apiPartialGuildToCurrentGuild, apiPartialGuildToPartialGuild, apiUserToCurrentUser } from "$lib/utils/formatting";
+import { redirect, type Handle, type ServerInit } from "@sveltejs/kit";
+import { Routes, type APIPartialGuild, type APIUser } from "discord-api-types/v10";
+
+export const init: ServerInit = async () => {
+  // DB Connection
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+  if (event.url.pathname.startsWith("/api")) {
+    const response = await resolve(event);
+    return response;
+  }
+
+  const token = event.cookies.get("discord-token");
+  if (!token) {
+    return redirect(401, "/login");
+  }
+
+  let verifedToken: FullCookieToken | null = null;
+  try {
+    const dToken = decodeToken(token);
+    verifedToken = verifyTokenPayload(dToken);
+    if (!verifedToken) {
+      throw { status: 401, redirect: "/login" };
+    }
+
+    const userData = await getUserData(verifedToken.access_token, event.fetch);
+
+    if (!userData) {
+      throw { status: 500, redirect: "/login" };
+    }
+    event.locals.currentUser = apiUserToCurrentUser(userData);
+  } catch (e: any) {
+    return redirect(e.status || 500, e.redirect || "/login");
+  }
+
+  // Fetch user guilds
+  try {
+    const guilds = await getUserGuilds(event.locals.currentUser.id, verifedToken.access_token, event.fetch);
+
+    if (!guilds) {
+      throw { status: 500, redirect: "/login" };
+    }
+
+    event.locals.guilds = guilds.map(apiPartialGuildToPartialGuild);
+  } catch (e: any) {
+    return redirect(e.status || 500, e.redirect || "/login");
+  }
+
+  // Add guild ID to locals
+  if (event.url.pathname != "/") {
+    const guildIdMatch = event.url.pathname.match(/^\/(\d+)\S+/);
+
+    if (guildIdMatch) {
+      const guildId = guildIdMatch[1];
+      const guild = event.locals.guilds.find((g) => g.id === guildId);
+
+      if (guild) {
+        // TODO: Fetch channels and roles for active guild from client API
+        /*
+          Any type is used here because it's hard to sync the types between the client api and dashboard
+          Typically there is at least `id`, `name`, `position` and `type` for channels
+          Typically there is at least `id`, `name`, `color` and `position` for roles
+        */
+        let channels: any[] = [];
+        let roles: any[] = [];
+
+        event.locals.currentGuild = apiPartialGuildToCurrentGuild(guild, channels, roles);
+      }
+    }
+  }
+
+  const response = await resolve(event);
+  return response;
+};
+
+// export async function handleError() {}
