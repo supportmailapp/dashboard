330c437f0c17217fbe46e2dc26ad42ea4db1155e
The-LukeZ <luke.hent3005@gmail.com>
2025-01-23 23:18:08 +0100

    feat(public constants): add OAuth2 URL generation for Discord authorization and token handling

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,36 @@
 // Public constants
 
+import { env } from '$env/dynamic/public';
+
 export const mediaQuery = {
   sm: 640,
   md: 768,
   lg: 1024,
   xl: 1280,
   xl2: 1536,
+} as const;
+
+interface AuthorizeUrlParams {
+  clientId: string;
+  scope: string;
+  state: string;
+  promt?: string;
+}
+
+export const urls = {
+  authorize: function ({ clientId, scope, state, promt = 'none' }: AuthorizeUrlParams): string {
+    return (
+      'https://discord.com/oauth2/authorize?' +
+      new URLSearchParams({
+        client_id: clientId,
+        response_type: 'code',
+        prompt: promt,
+        scope: scope,
+        redirect_uri: `${env.PUBLIC_discordRedirectUri}/discord/callback`,
+        state: state,
+      }).toString()
+    );
+  },
+  token: () => 'https://discord.com/api/oauth2/token',
+  revocation: () => 'https://discord.com/api/oauth2/token/revoke',
 } as const;
