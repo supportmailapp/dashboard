ee4a5007fac97246da57d1e2c2bb2463e8e8df24
The-LukeZ <luke.hent3005@gmail.com>
2025-01-24 16:01:08 +0100

    fix: change Discord auth route from POST to GET and log slug in page component

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,4 +2,6 @@
   import type { PageData } from "./$types";
 
   let { data }: { data: PageData } = $props();
+
+  console.log(data.slug);
 </script>
--- src/routes/api/v1/discord-auth/+server.ts
+++ src/routes/api/v1/discord-auth/+server.ts
@@ -1,4 +1,4 @@
 import { callbackHandler } from "$lib/discord/oauth2";
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const POST = callbackHandler satisfies RequestHandler;
+export const GET = callbackHandler satisfies RequestHandler;
