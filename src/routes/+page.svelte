f1b753bbdc86096024b0aeeabc09f000aaab1477
The-LukeZ <luke.hent3005@gmail.com>
2025-01-25 12:48:43 +0100

    feat(page): update layout to conditionally display user content or login option

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
+  import { page } from "$app/state";
   import Branding from "$lib/assets/Branding.svelte";
 
   let { data } = $props();
 </script>
 
 <main>
-  <div class="xy-center-items z-50 flex h-full w-full max-w-[1400px] flex-col md:gap-y-15 gap-y-5 md:h-[60%]">
+  <div
+    class="xy-center-items z-50 flex h-full w-max max-w-[{data.currentUser
+      ? '1400px'
+      : '500px'}] flex-col gap-y-5 md:h-[60%] md:gap-y-15"
+  >
     <Branding />
 
     {#if data.currentUser}
@@ -27,10 +32,20 @@
 
       <div class="bg-base-200 inset-shadow-md xy-center-items relative my-auto flex h-full w-full flex-col rounded-lg">
         <div class="flex flex-col gap-y-5 p-5">
-          <!-- Server Table -->
-          <div class="w-full text-clip">
-            <p>Placeholder</p>
-          </div>
+          {#if data.currentUser}
+            <!-- Server Table -->
+            <div class="w-full text-clip">
+              <p>Placeholder</p>
+            </div>
+          {:else}
+            <!-- Login with Discord -->
+            <form method="POST" action="?/login">
+              <button class="dy-btn dy-btn-lg dy-btn-soft gap-x-2 border-2">
+                <img src="/discord-mark-white.svg" alt="Discord Logo" class="h-8 w-8" />
+                <span>Login with Discord</span>
+              </button>
+            </form>
+          {/if}
         </div>
       </div>
     </div>
