3c8032ae1cf5bd2fb427651663d00b4d35a8d583
The-LukeZ <luke.hent3005@gmail.com>
2025-01-25 12:48:38 +0100

    feat(root-path): enhance load function to fetch user guilds and filter mutual guilds

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,21 +3,38 @@ import { decodeToken } from "$lib/server/auth.js";
 import "$env/static/private";
 import type { PageServerLoad } from "./$types";
 import { redirect, type Actions } from "@sveltejs/kit";
-import { loginHandler } from "$lib/discord/oauth2";
+import { getUserGuilds, loginHandler } from "$lib/discord/oauth2";
+import clientAPI from "$lib/server/clientApi";
+import { apiPartialGuildToPartialGuild } from "$lib/utils/formatting";
 
-export const load = async function ({ cookies, locals }) {
+export const load = async function ({ cookies, locals, url, fetch }) {
+  if (url.pathname == "/?logout=true") {
+    cookies.delete("discord-token", { path: "/" });
+    return redirect(302, "/");
+  }
+
   const cookieToken = cookies.get("discord-token");
-  if (!cookieToken) {
+  const tokenData = decodeToken(cookieToken, true);
+  if (!tokenData || !locals.currentUser) {
     return {};
   }
 
-  const tokenData = decodeToken(cookieToken);
-  if (!tokenData) {
-    return {};
+  // Fetch user guilds
+  if (!locals.guilds) {
+    const userGuilds = await getUserGuilds(locals.currentUser.id, tokenData.access_token, fetch);
+    if (!userGuilds) {
+      return {};
+    }
+
+    const mutualGuilds = await clientAPI.filterMutualGuilds(userGuilds.map((g) => g.id));
+    locals.guilds = userGuilds.reduce((acc, guild) => {
+      if (mutualGuilds.includes(guild.id)) {
+        acc.push(apiPartialGuildToPartialGuild(guild));
+      }
+      return acc;
+    }, [] as PartialGuild[]);
   }
 
-  // Fetch user data from Discord API
-  // Return server list
   return {
     guilds: locals.guilds,
     currentGuild: locals.currentGuild,
