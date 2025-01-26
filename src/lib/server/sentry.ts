2c86538edb34fbc32c315857baa095afa5e0f78b
The-LukeZ <luke.hent3005@gmail.com>
2025-01-26 12:49:01 +0100

    feat(sentry): add feedback integration with customizable options

--- src/lib/server/sentry.ts
+++ src/lib/server/sentry.ts
@@ -7,6 +7,14 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   release: env.version,
+
+  integrations: [
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+      isEmailRequired: true,
+    }),
+  ],
 });
 
 export default Sentry;
