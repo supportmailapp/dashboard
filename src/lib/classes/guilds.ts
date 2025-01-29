e193f18cf03cff63efe6d0a328f0b67fbe599727
The-LukeZ <luke.hent3005@gmail.com>
2025-01-29 21:49:30 +0100

    feat(guilds): enhance BasicGuild and ActiveGuild with permissions handling and static factory methods

--- src/lib/classes/guilds.ts
+++ src/lib/classes/guilds.ts
@@ -1,14 +1,27 @@
-class BasicGuild implements BaseGuild {
-  id: string;
-  name: string;
-  iconHash: string | null;
-  isConfigured: boolean;
+import type { RESTAPIPartialCurrentUserGuild } from "discord-api-types/v10";
 
-  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean) {
+class BasicGuild implements BaseGuild {
+  public readonly id: string;
+  public readonly name: string;
+  public readonly iconHash: string | null;
+  public isConfigured: boolean;
+  public readonly permissions: number | bigint;
+
+  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean, permissions: number | bigint) {
     this.id = id;
     this.name = name;
     this.iconHash = iconHash;
     this.isConfigured = isConfigured;
+    this.permissions = permissions;
+  }
+
+  public static from(guild: RESTAPIPartialCurrentUserGuild, configured: boolean = false): BasicGuild {
+    return new BasicGuild(guild.id, guild.name, guild.icon, configured, Number(guild.permissions));
+  }
+
+  public setConfigured(configured: boolean): BasicGuild {
+    this.isConfigured = configured;
+    return this;
   }
 }
 
@@ -16,10 +29,15 @@ class ActiveGuild extends BasicGuild implements IActiveGuild {
   public roles: BasicRole[];
   public channels: BasicChannel[];
 
-  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean) {
-    super(id, name, iconHash, isConfigured);
+  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean, permissions: number | bigint) {
+    super(id, name, iconHash, isConfigured, permissions);
     this.roles = [];
     this.channels = [];
+  }
+
+  public static override from(guild: RESTAPIPartialCurrentUserGuild | BaseGuild, configured: boolean = false): ActiveGuild {
+    const iconHash = "icon" in guild ? guild.icon : guild.iconHash;
+    return new ActiveGuild(guild.id, guild.name, iconHash, configured, Number(guild.permissions));
   }
 
   setRoles(roles: BasicRole[]): ActiveGuild {
