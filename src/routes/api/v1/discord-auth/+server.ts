e3e1b93b46cd0c6c4e491377df30a2b54fedcdce
The-LukeZ <luke.hent3005@gmail.com>
2025-01-29 21:54:04 +0100

    refactor(discord-auth): simplify GET handler by removing type satisfaction

--- src/routes/api/v1/discord-auth/+server.ts
+++ src/routes/api/v1/discord-auth/+server.ts
@@ -1,4 +1,3 @@
 import { callbackHandler } from "$lib/discord/oauth2";
-import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET = callbackHandler satisfies RequestHandler;
+export const GET = callbackHandler;
