a22428d006583c4ce76ae3796aafa98d27f78a67
The-LukeZ <luke.hent3005@gmail.com>
2025-01-31 13:01:29 +0100

    refactor(guilds): simplify guild class structure and enhance documentation

--- src/lib/classes/guilds.ts
+++ src/lib/classes/guilds.ts
@@ -1,54 +1,52 @@
 import type { RESTAPIPartialCurrentUserGuild } from "discord-api-types/v10";
 
-class BasicGuild implements BaseGuild {
+class Guild {
   public readonly id: string;
   public readonly name: string;
   public readonly iconHash: string | null;
   public isConfigured: boolean;
   public readonly permissions: number | bigint;
+  public roles?: BasicRole[];
+  public channels?: BasicChannel[];
 
-  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean, permissions: number | bigint) {
+  constructor(
+    id: string,
+    name: string,
+    iconHash: string | null,
+    isConfigured: boolean,
+    permissions: number | bigint,
+    roles?: BasicRole[],
+    channels?: BasicChannel[],
+  ) {
     this.id = id;
     this.name = name;
     this.iconHash = iconHash;
     this.isConfigured = isConfigured;
     this.permissions = permissions;
+    this.roles = roles || [];
+    this.channels = channels || [];
   }
 
-  public static from(guild: RESTAPIPartialCurrentUserGuild, configured: boolean = false): BasicGuild {
-    return new BasicGuild(guild.id, guild.name, guild.icon, configured, Number(guild.permissions));
+  /**
+   * Creates an instance of `Guild` from a given guild object.
+   *
+   * @param _guild - The guild object, which can be either a RESTAPIPartialCurrentUserGuild or a BaseGuild.
+   * @param configured - A boolean indicating whether the guild is configured. Defaults to false.
+   * @returns An instance of Guild.
+   */
+  public static from(_guild: RESTAPIPartialCurrentUserGuild, configured: boolean = false): Guild {
+    return new Guild(_guild.id, _guild.name, _guild.icon, configured, Number(_guild.permissions));
   }
 
-  public setConfigured(configured: boolean): BasicGuild {
-    this.isConfigured = configured;
-    return this;
-  }
-}
-
-class ActiveGuild extends BasicGuild implements IActiveGuild {
-  public roles: BasicRole[];
-  public channels: BasicChannel[];
-
-  constructor(id: string, name: string, iconHash: string | null, isConfigured: boolean, permissions: number | bigint) {
-    super(id, name, iconHash, isConfigured, permissions);
-    this.roles = [];
-    this.channels = [];
-  }
-
-  public static override from(guild: RESTAPIPartialCurrentUserGuild | BaseGuild, configured: boolean = false): ActiveGuild {
-    const iconHash = "icon" in guild ? guild.icon : guild.iconHash;
-    return new ActiveGuild(guild.id, guild.name, iconHash, configured, Number(guild.permissions));
-  }
-
-  setRoles(roles: BasicRole[]): ActiveGuild {
+  setRoles(roles: BasicRole[]): Guild {
     this.roles = roles;
     return this;
   }
 
-  setChannels(channels: BasicChannel[]): ActiveGuild {
+  setChannels(channels: BasicChannel[]): Guild {
     this.channels = channels;
     return this;
   }
 }
 
-export { BasicGuild, ActiveGuild };
+export { Guild };
