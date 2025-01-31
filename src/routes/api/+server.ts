6a6ad60081cdfca23ac6fa629ea6e12ddc4cfdb5
The-LukeZ <luke.hent3005@gmail.com>
2025-01-31 15:06:22 +0100

    fix(api): disable prerendering for the heartbeat endpoint

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,5 +1,7 @@
 import { type RequestHandler } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const GET: RequestHandler = async () => {
   return new Response("Heartbeat", { status: 200, statusText: "OK" });
 };
