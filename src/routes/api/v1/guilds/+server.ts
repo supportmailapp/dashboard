383f0d033e0888b5009f9fb3058a68cebe9ae346
The-LukeZ <luke.hent3005@gmail.com>
2025-01-31 13:09:11 +0100

    refactor(guilds): enhance GET handler to fetch guild details by ID and improve error handling

--- src/routes/api/v1/guilds/+server.ts
+++ src/routes/api/v1/guilds/+server.ts
@@ -1,14 +1,31 @@
+import { env } from "$env/dynamic/private";
+import { urls } from "$lib/constants";
 import { getUserGuilds } from "$lib/discord/oauth2";
 import { decodeToken } from "$lib/server/auth";
 import clientAPI from "$lib/server/clientApi";
 import { canManageBot } from "$lib/utils/permissions";
 import { error, json, type RequestHandler } from "@sveltejs/kit";
+import { Routes } from "discord-api-types/v10";
 
 export const GET: RequestHandler = async ({ cookies, fetch, url }) => {
   const searchParams = url.searchParams;
   const token = decodeToken(cookies.get("discord-token"), true);
   if (!token) {
     return error(401, "Unauthorized");
+  }
+
+  const guildId = searchParams.get("guild_id");
+  if (guildId) {
+    const guild = await fetch(urls.discordBase + Routes.guild(guildId), {
+      method: "GET",
+      headers: {
+        Authorization: `Bot ${env.discordBotToken}`,
+      },
+    });
+    if (!guild) {
+      return error(404, "Guild not found");
+    }
+    return json(guild);
   }
 
   let guilds = await getUserGuilds(token.userId, token.access_token, fetch, url.searchParams.get("bypass_cache") === "true");
@@ -26,14 +43,5 @@ export const GET: RequestHandler = async ({ cookies, fetch, url }) => {
     ...guild,
     isConfigured: validBotGuildIds.includes(guild.id),
   }));
-
-  if (searchParams.has("guild_id")) {
-    const guildId = searchParams.get("guild_id");
-    const guild = modifedGuilds.find((g) => g.id === guildId);
-    if (!guild) {
-      return error(404, "Guild not found");
-    }
-    return json(guild);
-  }
   return json(modifedGuilds);
 };
