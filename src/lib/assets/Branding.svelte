35d543c5767aeea0001b2dd020b09d809b0c7319
The-LukeZ <luke.hent3005@gmail.com>
2025-02-04 17:57:14 +0100

    style(branding): enhance branding layout with flexbox and drop shadow

--- src/lib/assets/Branding.svelte
+++ src/lib/assets/Branding.svelte
@@ -1,4 +1,4 @@
-<div id="branding" class="branding-container">
-  <img src="/logo.png" alt="Logo" class="h-16 w-16" />
+<div id="branding" class="flex flex-row items-center justify-center gap-x-3 select-none">
+  <img src="/logo.png" alt="Logo" class="h-16 w-16 drop-shadow-md" />
   <span class="text-3xl font-bold text-white sm:text-4xl">SupportMail</span>
 </div>
