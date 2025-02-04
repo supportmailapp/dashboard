0f49141e19a849d513dee3763579f6da57b708b3
The-LukeZ <luke.hent3005@gmail.com>
2025-02-04 16:52:24 +0100

    refactor(hooks): comment out error handling function for future review

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -9,14 +9,14 @@ if (!building) {
   });
 }
 
-export const handleError: HandleClientError = async ({ error, event, status, message }) => {
-  console.error("Client error", error);
+// export const handleError: HandleClientError = async ({ error, event, status, message }) => {
+//   console.error("Client error", error);
 
-  const errorId = Sentry.captureException(error, { extra: { event, status, message } });
-  console.log("Error ID:", errorId);
-  return {
-    status,
-    message: message || "Internal server error",
-    errorId,
-  };
-};
+//   const errorId = Sentry.captureException(error, { extra: { event, status, message } });
+//   console.log("Error ID:", errorId);
+//   return {
+//     status,
+//     message: message || "Internal server error",
+//     errorId,
+//   };
+// };
