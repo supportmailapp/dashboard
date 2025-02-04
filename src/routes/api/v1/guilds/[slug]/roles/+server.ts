58cbabb769d8918fd61567bdf0e8e73b5657ff11
The-LukeZ <luke.hent3005@gmail.com>
2025-02-04 16:53:40 +0100

    feat(guilds): add API endpoint to fetch guild roles with caching

---
+++ src/routes/api/v1/guilds/[slug]/roles/+server.ts
@@ -0,0 +1,32 @@
+import { getGuildRoles, setGuildRoles } from "$lib/cache/guilds";
+import { discordREST } from "$lib/discord/utils";
+import { apiRoleToBasic } from "$lib/utils/formatting";
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ params }) => {
+  const guildId = params.slug;
+
+  if (guildId) {
+    const cachedRoles = getGuildRoles(guildId);
+    if (cachedRoles) return Response.json(cachedRoles, { status: 200, statusText: "OK" });
+
+    try {
+      const roles = await discordREST.getGuildRoles(guildId);
+      setGuildRoles(
+        guildId,
+        roles.map((r) => apiRoleToBasic(r)),
+      );
+      return Response.json(roles, { status: 200, statusText: "OK" });
+    } catch (err: any) {
+      return Response.json(
+        {
+          message: err.message || "Internal Server Error",
+          details: err,
+        },
+        { status: err.status || 500, statusText: err.statusText || "Internal Server Error" },
+      );
+    }
+  }
+
+  return Response.json("Bad Request", { status: 400, statusText: "Bad Request" });
+};
