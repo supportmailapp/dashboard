4d6cee8890d0a5f43468b7d5db107b27f7dbdc71
The-LukeZ <luke.hent3005@gmail.com>
2025-02-04 16:56:54 +0100

    feat(cache): enhance guild caching with separate stores for roles and channels, and improve data handling

--- src/lib/cache/guilds.ts
+++ src/lib/cache/guilds.ts
@@ -1,105 +1,117 @@
 import type { RESTAPIPartialCurrentUserGuild } from "discord-api-types/v10";
 import NodeCache from "node-cache";
 
-// All the BasicGuilds currently in the cache
-const guildsCache = new NodeCache({
-  stdTTL: 900,
-  checkperiod: 30,
+const BASE_CACHE_OPTS: NodeCache.Options = {
+  stdTTL: 3600,
+  checkperiod: 60,
   errorOnMissing: false,
-});
+} as const;
 
-type GuildsCacheValue = RESTAPIPartialCurrentUserGuild | undefined;
+const guildsStore = new NodeCache(BASE_CACHE_OPTS);
 
-// Maps a user ID to an array of guilds the user is in and an array of guilds the user has configured
-// Format: `{accessToken}-{userId}` -> { guildIds: string[], configured: string[] }
-const userGuildsStore = new NodeCache({
-  stdTTL: 600,
-  checkperiod: 30,
-  errorOnMissing: false,
-});
+const guildRolesStore = new NodeCache(BASE_CACHE_OPTS);
 
-type UserGuildsMappingValue = {
-  guildIds: string[];
-  configured: string[];
-};
+const userGuildsStore = new NodeCache({ ...BASE_CACHE_OPTS, stdTTL: 1800 });
 
-function buildUserKey(userId: string, accessToken: string): string {
-  return `${userId}:${accessToken}`;
+function buildKey(typ: string, guildId: string): `${string}:${string}` {
+  return `${typ}:${guildId}`;
 }
 
-/**
- * Sets the user's guilds in the userGuildsStore and caches the guilds in the guildsCache.
- *
- * @param userId - The ID of the user.
- * @param accessToken - The access token of the user.
- * @param guilds - An array of partial guild objects associated with the user.
- */
-export function setUserWithGuilds(
-  userId: string,
-  accessToken: string,
-  guilds: RESTAPIPartialCurrentUserGuild[],
-  configured: string[] | null = null,
-): void {
-  const userKey = buildUserKey(userId, accessToken);
-  const userGuildsData = userGuildsStore.get<UserGuildsMappingValue>(userKey);
-
-  userGuildsStore.set(userKey, {
-    guildIds: guilds.map((g) => g.id),
-    configured: configured ?? userGuildsData?.configured ?? [],
-  });
-
-  guildsCache.mset(
-    guilds.map((guild) => ({
-      key: guild.id,
-      val: guild,
-    })),
-  );
-}
-
-type GetUserGuildsResult = {
-  guilds: RESTAPIPartialCurrentUserGuild[];
-  configured: string[];
-};
-
-/**
- * Retrieves the guilds associated with the user from the cache.
- *
- * @param userId - The unique identifier of the user.
- * @param accessToken - The access token of the user.
- * @param guildIds - An array of partial guild objects representing the guilds the user is a member of.
- */
-export function getUserGuilds(userId: string, accessToken: string): GetUserGuildsResult | null {
-  const userGuildsData = userGuildsStore.get<UserGuildsMappingValue>(buildUserKey(userId, accessToken));
-  if (!userGuildsData) {
-    return null;
-  }
-
-  const guildsList = guildsCache.mget<GuildsCacheValue | undefined>(userGuildsData.guildIds);
-  if (!guildsList || Object.values(guildsList).length != userGuildsData.guildIds.length) {
-    return null; // If any of the guilds are missing, return null to fetch the user's guilds again
-  }
-
+export function parseToDCGuild(guild: RESTAPIPartialCurrentUserGuild, configured = false): DCGuild {
   return {
-    guilds: Object.values(guildsList) as RESTAPIPartialCurrentUserGuild[],
-    configured: userGuildsData.configured,
+    id: guild.id,
+    name: guild.name,
+    iconHash: guild.icon,
+    isConfigured: configured,
+    permissions: BigInt(guild.permissions),
   };
 }
 
-export function updateGuilds(guilds: RESTAPIPartialCurrentUserGuild[]): void {
-  guildsCache.mset(
+// * Store object data stringified, because Objects have a higher memory footprint and the obejcts are relatively small.
+
+// Setters //
+export function overwriteUserGuilds(userId: string, guilds: DCGuild[]): void {
+  userGuildsStore.set(userId, JSON.stringify(guilds.map((guild) => guild.id)));
+  guildsStore.mset(
     guilds.map((guild) => ({
       key: guild.id,
-      val: guild,
+      val: JSON.stringify(guild),
     })),
   );
 }
 
-/**
- * Deletes all entries in the userGuildsStore that end with the specified userId.
- *
- * @param userId - The ID of the user whose guild entries should be deleted.
- */
-export function del(userId: string): void {
-  const keys = userGuildsStore.keys().filter((key) => key.endsWith(userId));
-  userGuildsStore.del(keys);
+export function setGuildRoles(guildId: string, roles: BasicRole[]): void {
+  guildsStore.set(buildKey("roles", guildId), JSON.stringify(roles));
+}
+
+export function setGuildChannels(guildId: string, channels: BasicChannel[]): void {
+  guildsStore.set(buildKey("channels", guildId), JSON.stringify(channels));
+}
+
+// Getters //
+export function getGuild(id: string): DCGuild | null {
+  const guild = guildsStore.get(id) as any;
+  if (!guild) return null;
+  return JSON.parse(guild);
+}
+
+export function getGuildRoles(guildId: string): BasicRole[] | null {
+  const roles = guildsStore.get(buildKey("roles", guildId)) as any;
+  if (!roles) return null;
+  return JSON.parse(roles);
+}
+
+export function getGuildChannels(guildId: string): BasicChannel[] | null {
+  const channels = guildsStore.get(buildKey("channels", guildId)) as any;
+  if (!channels) return null;
+  return JSON.parse(channels);
+}
+
+export function getUserGuilds(userId: string): DCGuild[] | null {
+  const guildIds = userGuildsStore.get(userId) as any;
+  if (!guildIds) return null;
+  const guilds = guildsStore.mget<string>(guildIds);
+  if (!guilds) return null;
+  return Object.values(guilds).map((guild) => JSON.parse(guild));
+}
+
+// Deleters //
+export function delGuild(id: string): void {
+  guildsStore.del(id);
+}
+
+export function delGuildRoles(guildId: string): void {
+  guildsStore.del(buildKey("roles", guildId));
+}
+
+export function delGuildChannels(guildId: string): void {
+  guildsStore.del(buildKey("channels", guildId));
+}
+
+export function delUserGuilds(userId: string): void {
+  userGuildsStore.del(userId);
+}
+
+// Clearers //
+export function clearGuilds(): void {
+  guildsStore.flushAll();
+}
+
+export function clearGuildRoles(): void {
+  guildRolesStore.flushAll();
+}
+
+export function clearGuildChannels(): void {
+  guildsStore.flushAll();
+}
+
+export function clearUserGuilds(): void {
+  userGuildsStore.flushAll();
+}
+
+export function clearAll(): void {
+  guildsStore.flushAll();
+  guildRolesStore.flushAll();
+  guildsStore.flushAll();
+  userGuildsStore.flushAll();
 }
