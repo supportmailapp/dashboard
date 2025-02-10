781965c3ffe8c55f544889163de272178338805a
The-LukeZ <luke.hent3005@gmail.com>
2025-02-10 20:25:19 +0100

    feat(layout): enhance layout with navigation transitions and improve guild data loading logic

--- src/routes/[slug]/+layout.svelte
+++ src/routes/[slug]/+layout.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { page } from "$app/state";
+  import { env } from "$env/dynamic/public";
   import DesktopNavigation from "$lib/components/DesktopNavigation.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import MobileNavigation from "$lib/components/MobileNavigation.svelte";
   import NavigationDialog from "$lib/components/NavigationDialog.svelte";
-  import { APIRoutes, mediaQuery } from "$lib/constants.js";
-  import { gg, loadGuildData, resetGuild } from "$lib/stores/guild.svelte";
+  import { APIRoutes, mediaQuery, urls } from "$lib/constants.js";
+  import { gg, loadGuildData } from "$lib/stores/guild.svelte";
   import { site } from "$lib/stores/site.svelte.js";
   import { onMount } from "svelte";
+  import { slide } from "svelte/transition";
 
   let { children, data } = $props();
 
   let innerWidth = $state(800);
 
   onMount(async function () {
-    if (page.params.slug !== gg.guild?.id) {
+    if (!gg.guild || page.params.slug !== gg.guild.id) {
       await loadGuildData(page.params.slug);
     }
+
     if (!site.news) {
       const res = await fetch(APIRoutes.news(), {
         method: "GET",
@@ -41,7 +45,7 @@
   {/if}
 
   <div class="w-full">
-    <div class="flex min-h-screen flex-col p-3 text-wrap">
+    <div class="flex min-h-screen flex-col p-3 text-wrap" transition:slide={{ duration: 250, axis: "x" }}>
       {@render children()}
     </div>
     <Footer year={data.ccDate} />
