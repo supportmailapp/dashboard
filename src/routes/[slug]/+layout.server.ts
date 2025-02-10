84cdf44eddf13ab2d918d4bbd16335e763ea6e51
The-LukeZ <luke.hent3005@gmail.com>
2025-02-10 11:05:57 +0100

    refactor(routes): remove unused layout and page server files

--- src/routes/[slug]/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-export const load = async ({ locals }) => {
-  if (!locals.user) redirect(302, "/?logout=true");
-
-  return {
-    user: locals.user,
-  };
-};
