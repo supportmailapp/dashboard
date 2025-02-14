283734f763c44a7d1fed5ab9fd234b142e274240
The-LukeZ <luke.hent3005@gmail.com>
2025-02-14 14:46:13 +0100

    feat(button): add RefreshButton component with customizable click functionality

---
+++ src/lib/components/RefreshButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { MouseEventHandler } from "svelte/elements";
+
+  const defaultClickFunc = () => {
+    window.location.assign(whatInvalidate);
+  };
+
+  type Props = {
+    /**
+     * The text to display on the button.
+     */
+    text: string;
+    /**
+     * The path to invalidate.
+     */
+    whatInvalidate?: string | URL;
+    /**
+     * The function to run when the button is clicked. Only use this if you need to run a function instead of navigating.
+     */
+    clickFunction?: MouseEventHandler<HTMLButtonElement>;
+  };
+  let { text, whatInvalidate = "/", clickFunction = defaultClickFunc }: Props = $props();
+</script>
+
+<button class="dy-btn dy-btn-md md:dy-btn-sm dy-btn-soft flex justify-between gap-x-2 border-2 px-2" onclick={clickFunction}>
+  <img src="/arrow-update.svg" alt="Refresh" class="block size-5" />
+  {text}
+</button>
