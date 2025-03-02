5bca4bd3e5ab3583c765d9f0a03061741e8a0f1b
The-LukeZ <luke.hent3005@gmail.com>
2025-03-02 03:28:05 +0100

    feat(page): remove unused session management logic and clean up code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,19 +1,1 @@
-import { redirect, type Actions } from "@sveltejs/kit";
-
 export const prerender = false;
-
-// const valkey = new Valkey(); // TODO: use this to cache guilds in a seperate file
-
-export const load = async ({ cookies, url }) => {
-  if (url.pathname == "/?logout=true") {
-    cookies.delete("session", { path: "/" });
-    return {};
-  }
-};
-
-export const actions = {
-  logout: async ({ cookies }) => {
-    cookies.delete("session", { path: "/" });
-    return redirect(302, "/");
-  },
-} satisfies Actions;
