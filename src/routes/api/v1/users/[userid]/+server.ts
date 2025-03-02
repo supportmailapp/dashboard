97b5d71da4d42fba35ce8e2f7a6f8831ccddfdd0
The-LukeZ <luke.hent3005@gmail.com>
2025-03-02 03:29:05 +0100

    feat(api): enhance user data retrieval and validation logic

--- src/routes/api/v1/users/[userid]/+server.ts
+++ src/routes/api/v1/users/[userid]/+server.ts
@@ -1,21 +1,28 @@
-import { fetchUserData } from "$lib/discord/oauth2.js";
+import { ErrorResponses, LANGUAGES } from "$lib/constants.js";
 import { getUser, updateUser } from "$lib/server/db/utils.js";
-import { getUser as getCachedUser } from "$lib/cache/users.js";
 
-export const GET = async ({ params }) => {
+export const GET = async ({ params, locals }) => {
+  if (locals.userId !== params.userid) {
+    return ErrorResponses.forbidden();
+  }
+
   const dbUser = await getUser(params.userid);
   if (dbUser) {
     delete dbUser.accessToken;
+    // @ts-ignore | This is valid.
     delete dbUser.updatedAt;
     return Response.json(dbUser);
   }
-  return new Response("User not found", { status: 404 });
+  return ErrorResponses.notFound("User not found");
 };
 
-const ALLOWED_UPDATE_KEYS = ["language", "autoRedirect", "t_left", "tips"];
+const ALLOWED_UPDATE_KEYS = ["language", "autoRedirect", "tips"];
 
-export const PATCH = async ({ params, request, fetch }) => {
+export const PATCH = async ({ params, request, locals }) => {
   const data = await request.json();
+  if (locals.userId !== data.id || locals.userId !== params.userid) {
+    return ErrorResponses.forbidden();
+  }
   // Filter out invalid keys
   for (const key in data) {
     if (!ALLOWED_UPDATE_KEYS.includes(key)) {
@@ -23,19 +30,26 @@ export const PATCH = async ({ params, request, fetch }) => {
     }
   }
 
+  // check if the values are valid
+  if (
+    !Object.values(LANGUAGES)
+      .map((l) => l.value)
+      .includes(data.language)
+  ) {
+    return ErrorResponses.badRequest("Invalid language");
+  }
+  if (typeof data.autoRedirect !== "boolean") {
+    return ErrorResponses.badRequest("Invalid autoRedirect");
+  }
+  if (typeof data.tips !== "boolean") {
+    return ErrorResponses.badRequest("Invalid tips");
+  }
+
   await updateUser(params.userid, data);
   const updated = await getUser(params.userid);
   if (!updated) {
-    return new Response("User not found", { status: 404 });
+    return ErrorResponses.notFound("User not found");
   }
 
-  const cached = getCachedUser(params.userid);
-  if (cached) {
-    return Response.json({ ...updated, ...cached });
-  }
-
-  const user = await fetchUserData(params.userid, fetch, false);
-  if (user) {
-    return Response.json({ ...updated, ...user });
-  }
+  return Response.json(updated);
 };
