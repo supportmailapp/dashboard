97ea0ff65a50c2d614e9399e12aeaf19f3c0534d
The-LukeZ <luke.hent3005@gmail.com>
2025-03-02 03:27:40 +0100

    feat(client): remove unused userSaveFunction and related imports

--- src/lib/utils/clientStuff.ts
+++
@@ -1,9 +0,0 @@
-import { site } from "$lib/stores/site.svelte";
-
-export async function userSaveFunction(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
-  event.preventDefault();
-  site.saving = true;
-  const form = event.currentTarget;
-  const formData = new FormData(form);
-  console.log("formData", formData);
-}
