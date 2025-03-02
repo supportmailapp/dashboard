2205b53969124a144367cd8a3755506564584b5f
The-LukeZ <luke.hent3005@gmail.com>
2025-03-02 03:27:56 +0100

    feat(layout): enhance user redirection logic and improve loading behavior for unauthenticated users

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,38 @@
   import { loadDbUser, user } from "$lib/stores/user.svelte";
   import { onMount } from "svelte";
   import "../app.css";
+  import { goto } from "$app/navigation";
 
   let { children, data } = $props();
 
-  user.discord = data.user as BasicUser;
+  user.discord = data.user;
 
   onMount(async function () {
-    console.log($state.snapshot(user.discord));
+    if (!data.user) {
+      const newUrl = new URL("/login", window.origin);
+      if (!window.location.pathname.match(/^\/(login)?/i)) {
+        if (window.location.pathname.match(/^\/\d+(\/\w+)?$/i)) {
+          // Redirect to the current page after login
+          newUrl.searchParams.set("redirect_base", window.location.pathname);
+        } else if (window.location.pathname.match(/^\/\w+/i)) {
+          // Redirect to the current page after selecting a server
+          newUrl.searchParams.set("redirect_guild", "/");
+        }
+      }
+      goto(newUrl.toString());
+      return;
+    }
+
     if (user.discord && !guilds.value.length) {
-      loadDbUser(user.discord.id);
       loadGuilds(user.discord.id);
+      loadDbUser(user.discord.id);
+    }
+
+    const redirect = window.localStorage.getItem("redirect_base");
+    if (window.location.pathname == "/" && redirect) {
+      window.localStorage.removeItem("redirect_base");
+      window.localStorage.removeItem("redirect_guild");
+      goto(redirect);
     }
   });
 </script>
