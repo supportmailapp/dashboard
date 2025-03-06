a7e8b9413838c94dd910dadf33c303bba4efb157
The-LukeZ <luke.hent3005@gmail.com>
2025-03-06 19:47:13 +0100

    feat(add): implement redirect logic for adding guilds with client authentication

---
+++ src/routes/add/[guildid]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { env } from "$env/dynamic/public";
+  import { urls } from "$lib/constants.js";
+  import { page } from "$app/state";
+
+  onMount(() => {
+    if (page.params.guildid) {
+      window.localStorage.setItem(
+        "redirect",
+        JSON.stringify({
+          path: `/`,
+          guildId: page.params.guildid,
+        } satisfies RedirectData),
+      );
+    }
+    return window.location.assign(urls.botAuth(env.PUBLIC_ClientId, page.params.guildid));
+  });
+</script>
