2b76e46dd17f3af25ea7400f4d2e902620f3b526
The-LukeZ <luke.hent3005@gmail.com>
2025-03-07 18:35:22 +0100

    feat(guild-page): integrate news section with alerts and improve layout
    - Add news section to guild page with dynamic alert-integration
    - remove unnecessary dividers and use gap from layout instead
    - fix plugin layout (widths and stuff)
    - make an own class for the plugin card because of the length of the class name

--- src/routes/g/[guildid]/+page.svelte
+++ src/routes/g/[guildid]/+page.svelte
@@ -5,12 +5,13 @@
   import RefreshButton from "$lib/components/RefreshButton.svelte";
   import { PLUGINS } from "$lib/constants";
   import { gg } from "$lib/stores/guild.svelte";
-  import { site } from "$lib/stores/site.svelte";
   import { user } from "$lib/stores/user.svelte";
   import { cdnUrls } from "$lib/utils/formatting";
+  import { CircleCheck, CircleX, Info, MessageSquareWarning, TriangleAlert } from "@lucide/svelte";
 
   let serverIdCopied = $state(false);
   let serverIdText = $derived(serverIdCopied ? "Copied!" : "Copy Server ID");
+  let news = $state<News[]>(page.data.news || []);
 
   $effect(() => {
     console.log("guild", $state.snapshot(gg.guild));
@@ -20,71 +21,78 @@
 </script>
 
 {#if gg.guild && user.discord}
-  <div style="padding: 1rem;">
-    <div class="flex h-30 flex-row items-center justify-start gap-5">
-      <img src={cdnUrls.guildIcon(gg.guild.id, gg.guild.iconHash, "512")} alt="Server icon" class="size-30" />
-      <div>
-        <div class="flex flex-row items-center justify-start gap-3">
-          <div
-            class="dy-tooltip dy-tooltip-right {serverIdCopied ? 'dy-tooltip-success' : 'dy-tooltip-accent'}"
-            data-tip={serverIdText}
+  <div class="flex w-full flex-row items-center justify-start gap-5">
+    <img src={cdnUrls.guildIcon(gg.guild.id, gg.guild.iconHash, "512")} alt="Server icon" class="size-20" />
+    <div>
+      <div class="flex flex-row items-center justify-start gap-3">
+        <div
+          class="dy-tooltip md:dy-tooltip-right dy-tooltip-bottom {serverIdCopied ? 'dy-tooltip-success' : 'dy-tooltip-accent'}"
+          data-tip={serverIdText}
+        >
+          <button
+            class="server-id-button"
+            onclick={() => {
+              if (serverIdCopied || !gg.guild) return;
+              navigator.clipboard.writeText(gg.guild.id);
+              serverIdCopied = true;
+              setTimeout(() => {
+                serverIdCopied = false;
+              }, 2000);
+            }}
           >
-            <button
-              class="text-primary cursor-pointer p-1 text-4xl font-bold underline underline-offset-4 transition-all duration-150 ease-linear"
-              onclick={() => {
-                if (serverIdCopied || !gg.guild) return;
-                navigator.clipboard.writeText(gg.guild.id);
-                serverIdCopied = true;
-                setTimeout(() => {
-                  serverIdCopied = false;
-                }, 2000);
-              }}
-            >
-              {gg.guild.name}
-            </button>
-          </div>
+            {gg.guild.name}
+          </button>
         </div>
-        <div class="h-3"></div>
-        <h2 class="pl-1 text-3xl font-semibold text-white">
-          Welcome <span class="text-amber-300">{user.discord?.displayName}</span>!
-        </h2>
+      </div>
+      <div class="h-3"></div>
+      <h2 class="pl-1 text-xl font-semibold text-white">
+        Welcome <span class="text-amber-300">{user.discord?.displayName}</span>!
+      </h2>
+    </div>
+  </div>
+  <span class="dy-divider dy-divider-neutral my-0"></span>
+  {#if news.length > 0}
+    <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+    <div tabindex="0" class="dy-collapse dy-collapse-arrow bg-base-300/60 mb-3">
+      <input type="checkbox" class="news-peer" checked />
+      <div class="dy-collapse-title flex flex-row items-center gap-2 font-semibold text-white">News</div>
+      <div class="dy-collapse-content text-primary-content flex flex-col gap-1.5">
+        {#each news as news}
+          <div role="alert" class="dy-alert dy-alert-horizontal {`dy-alert-${news.type}`}">
+            {#if news.type === "info"}
+              <Info color="currentColor" />
+            {:else if news.type === "success"}
+              <CircleCheck color="currentColor" />
+            {:else if news.type === "warning"}
+              <TriangleAlert color="currentColor" />
+            {:else}
+              <CircleX color="currentColor" />
+            {/if}
+            <div>
+              <h3 class="font-bold">{news.title}</h3>
+              <div class="text-xs">{news.content}</div>
+            </div>
+          </div>
+        {/each}
       </div>
     </div>
-    <div class="h-3"></div>
-    {#if site.news && site.news.length > 0}
-      <h2 class="text-2xl font-semibold text-white underline-offset-2">News</h2>
-      <div class="h-3"></div>
-      {#each site.news as news}
-        <div class="flex flex-col items-center justify-start gap-3">
-          <h3 class="text-xl font-semibold text-white">{news.title}</h3>
-          <p class="text-white">{news.content}</p>
-        </div>
-      {/each}
-      <div class="h-5"></div>
-      <span class="dy-divider dy-divider-primary"></span>
-    {/if}
-    <div class="h-5"></div>
-    <h2 class="text-3xl font-semibold text-white underline-offset-2 select-none">Plugins</h2>
-    <div class="h-3"></div>
-    <div class="flex w-full flex-row gap-2">
-      {#each PLUGINS as plugin}
-        <button
-          onclick={() => goto(buildNavHref(plugin.slug))}
-          class="dy-card bg-base-200 shadow-base-300 w-84 cursor-pointer items-start justify-start p-2 transition-all duration-150 ease-linear hover:shadow-md sm:p-4"
-        >
-          <div class="dy-card-body p-3">
-            <h2 class="flex items-center gap-0.5 font-semibold text-white">
-              <img src={plugin.iconUrl} alt="{plugin.name} icon" class="size-8" />
-              {plugin.name}
-              {#if plugin.isNew}
-                <div class="dy-badge dy-badge-secondary rounded-full">NEW</div>
-              {/if}
-            </h2>
-            <p class="text-start">{plugin.description}</p>
-          </div>
+  {/if}
+  <h2 class="text-3xl font-semibold text-white underline-offset-2 select-none">Plugins</h2>
+  <div class="flex w-full flex-wrap">
+    {#each PLUGINS as plugin}
+      <div class="flex w-full p-2 sm:w-1/2 md:pl-0">
+        <button onclick={() => goto(buildNavHref(plugin.slug))} class="plugin-card">
+          <h1 class="flex items-center gap-1 font-semibold text-white">
+            <img src={plugin.iconUrl} alt="{plugin.name} icon" class="aspect-square size-9" />
+            <span>{plugin.name}</span>
+            {#if plugin.isNew}
+              <div class="dy-badge dy-badge-accent rounded-full">NEW</div>
+            {/if}
+          </h1>
+          <p>{plugin.description}</p>
         </button>
-      {/each}
-    </div>
+      </div>
+    {/each}
   </div>
 {:else}
   <div class="flex h-full w-full items-center justify-center">
