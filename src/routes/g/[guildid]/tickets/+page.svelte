0cad6769b8e5451f946a00c9cd48e8a93cfc94c9
The-LukeZ <luke.hent3005@gmail.com>
2025-03-09 14:33:49 +0100

    feat(page): refactor ticket configuration layout and improve accessibility

--- src/routes/g/[guildid]/tickets/+page.svelte
+++ src/routes/g/[guildid]/tickets/+page.svelte
@@ -87,138 +87,146 @@ Page Contents:
 </script>
 
 <h1 class="my-2 font-bold text-amber-400">Ticket Configuration</h1>
-<span class="h-2"></span>
 
 {#if oldConfig && newConfig}
   <!-- 1. Enabled -->
-  <fieldset id="status" class="settings-container">
-    <h2 class="dy-fieldset-legend w-fit text-xl">Status</h2>
-    <label class="dy-fieldset-label w-fit py-1 text-base text-white">
-      <input
-        id="enabled-switch"
-        type="checkbox"
-        class="dy-toggle dy-toggle-success"
-        bind:checked={newConfig.enabled}
-        disabled={!!oldConfig.pausedUntil}
-      />{newConfig.enabled ? "Enabled" : "Disabled"}
-    </label>
-    {#if newConfig.enabled == false || typeof oldConfig.pausedUntil == "string"}
-      <div role="alert" class="dy-alert dy-alert-error dy-alert-soft mb-2" transition:slide={{ duration: 100, axis: "y" }}>
-        <div>
-          <h1>Ticketing is currently disabled.</h1>
-          {#if newConfig.enabled == false}
-            <p class="text-base-content">
-              No tickets can be opened at the moment, but currently open tickets are still active and can be used to exchange
-              messages.
-            </p>
-          {:else if oldConfig.pausedUntil}
-            <p class="text-base-content">
-              Ticketing is paused until {new Date(oldConfig.pausedUntil).toLocaleString(navigator.language, {
-                dateStyle: "medium",
-                timeStyle: "long",
-              })}.<br />
-              See <a href="#paused-until">Paused Until</a> for more information.
-            </p>
-          {/if}
-        </div>
-      </div>
-    {/if}
-  </fieldset>
-  <!-- The Rest -->
-  <div
-    class="flex w-full flex-col transition-opacity duration-150 select-none {newConfig.enabled
-      ? ''
-      : 'cursor-not-allowed opacity-60 *:pointer-events-none'}"
-  >
-    <!-- 2. Paused Until -->
-    <fieldset id="paused-until" class="settings-container">
-      <div class="flex flex-row overflow-visible">
-        <h2 class="dy-fieldset-legend w-fit text-xl">Paused Until</h2>
-        <div class="dy-tooltip dy-tooltip-top dy-tooltip-info flex items-center" data-tip={LongTooltips.pausedUntil_tickets}>
-          <button class="dy-btn dy-btn-circle dy-btn-sm items-center p-0">
-            <img src="/icons/question-mark-circle.svg" alt="Help" class="size-6" />
-          </button>
-        </div>
-      </div>
-      <div class="flex w-full max-w-lg flex-col gap-4 py-4">
-        <div class="dy-join dy-join-vertical md:dy-join-horizontal h-full">
-          <button
-            onclick={() => {
-              pauseInput.value = null;
-              pauseInput.fail = null;
-              pauseInput.type = pauseInput.type === "date" ? "duration" : "date";
-            }}
-            class="dy-btn dy-join-item btn-base-300 w-full md:max-w-25"
+  <div class="settings-container w-full">
+    <fieldset id="status" class="dy-fieldset">
+      <legend>Status</legend>
+      <div>
+        <label class="dy-fieldset-label w-fit text-base text-white">
+          <input
+            id="enabled-switch"
+            type="checkbox"
+            class="dy-toggle dy-toggle-success"
+            bind:checked={newConfig.enabled}
             disabled={!!oldConfig.pausedUntil}
-          >
-            <img src="/icons/arrow-update.svg" alt="Swap Inputs" class="size-5" />
-            {pauseInput.type === "date" ? "Duration" : "Date"}
-          </button>
-          {#if pauseInput.type == "date"}
-            <input
-              type="datetime-local"
-              class="dy-input dy-join-item w-full text-base"
-              onchangecapture={pauseInputHandler}
-              min={dayjs().format("YYYY-MM-DDTHH:mm")}
-              disabled={!!oldConfig.pausedUntil}
-            />
-          {:else}
-            <input
-              type="text"
-              class="dy-input dy-join-item w-full"
-              placeholder="Duration | Ex: 1d 2h 30m"
-              onchangecapture={pauseInputHandler}
-              disabled={!!oldConfig.pausedUntil}
-            />
-          {/if}
-        </div>
-
-        <!-- Display the date + time the pause will last. -->
-        <div role="alert" class="dy-alert dy-alert-info dy-alert-soft w-full p-2">
-          <span class="font-mono font-semibold">
-            {#if pauseInput.value}
-              When saved, Tickets will be paused until {new Date(pauseInput.value).toLocaleString(navigator.language, {
-                dateStyle: "medium",
-                timeStyle: "long",
-              })}.
-            {:else}
-              {pauseInput.type === "date" ? "Select a date and time." : "Enter a duration."}
-            {/if}
-          </span>
-        </div>
-
-        <div class="flex w-full grow flex-row gap-2">
-          <button
-            class="dy-btn dy-btn-accent grow"
-            disabled={!pauseInput}
-            onclick={() => {
-              pauseInput.value = null;
-              pauseInput.fail = null;
-            }}
-          >
-            Reset
-          </button>
-          <button
-            class="dy-btn dy-btn-primary grow"
-            disabled={!!oldConfig.pausedUntil || pauseInput.fail !== null}
-            onclick={() => {
-              gg.unsavedChanges = true;
-            }}
-          >
-            Save
-          </button>
-        </div>
+          />{newConfig.enabled ? "Enabled" : "Disabled"}
+        </label>
       </div>
 
-      {#if pauseInput.fail}
-        <div role="alert" class="dy-alert dy-alert-error dy-alert-soft my-2">
+      {#if newConfig.enabled == false || typeof oldConfig.pausedUntil == "string"}
+        <div role="alert" class="dy-alert dy-alert-error dy-alert-soft mb-2" transition:slide={{ duration: 100, axis: "y" }}>
           <div>
-            <h1>Invalid Input</h1>
-            <p class="text-base-content">{@html pauseInput.fail}</p>
+            <h1>Ticketing is currently disabled.</h1>
+            {#if newConfig.enabled == false}
+              <p class="text-base-content">
+                No tickets can be opened at the moment, but currently open tickets are still active and can be used to exchange
+                messages.
+              </p>
+            {:else if oldConfig.pausedUntil}
+              <p class="text-base-content">
+                Ticketing is paused until {new Date(oldConfig.pausedUntil).toLocaleString(navigator.language, {
+                  dateStyle: "medium",
+                  timeStyle: "long",
+                })}.<br />
+                See <a href="#paused-until">Paused Until</a> for more information.
+              </p>
+            {/if}
           </div>
         </div>
       {/if}
     </fieldset>
+  </div>
+
+  <!-- The Rest -->
+  <div class="w-full {newConfig.enabled ? '' : 'cursor-not-allowed opacity-60 *:pointer-events-none'}">
+    <!-- 2. Paused Until -->
+
+    <div class="settings-container">
+      <fieldset id="paused-until">
+        <legend>Paused Until</legend>
+        <div class="flex w-full max-w-lg flex-col gap-4">
+          <div class="flex flex-row justify-start gap-2">
+            <div class="dy-join dy-join-vertical md:dy-join-horizontal h-full w-full">
+              <button
+                onclick={() => {
+                  pauseInput.value = null;
+                  pauseInput.fail = null;
+                  pauseInput.type = pauseInput.type === "date" ? "duration" : "date";
+                }}
+                class="dy-btn dy-join-item btn-base-300 w-full md:max-w-25"
+                disabled={!!oldConfig.pausedUntil}
+              >
+                <img src="/icons/arrow-update.svg" alt="Swap Inputs" class="size-5" />
+                {pauseInput.type === "date" ? "Duration" : "Date"}
+              </button>
+              {#if pauseInput.type == "date"}
+                <input
+                  type="datetime-local"
+                  class="dy-input dy-join-item w-full text-base"
+                  onchangecapture={pauseInputHandler}
+                  min={dayjs().format("YYYY-MM-DDTHH:mm")}
+                  disabled={!!oldConfig.pausedUntil}
+                />
+              {:else}
+                <input
+                  type="text"
+                  class="dy-input dy-join-item w-full"
+                  placeholder="Duration | Ex: 1d 2h 30m"
+                  onchangecapture={pauseInputHandler}
+                  disabled={!!oldConfig.pausedUntil}
+                />
+              {/if}
+            </div>
+
+            <div
+              class="dy-tooltip dy-tooltip-top dy-tooltip-info ml-auto flex items-center"
+              data-tip={LongTooltips.pausedUntil_tickets}
+            >
+              <button class="dy-btn dy-btn-circle dy-btn-sm items-center p-0">
+                <img src="/icons/question-mark-circle.svg" alt="Help" class="size-6" />
+              </button>
+            </div>
+          </div>
+
+          <!-- Display the date + time the pause will last. -->
+          <div role="alert" class="dy-alert dy-alert-info dy-alert-soft w-full p-2">
+            <span class="font-mono font-semibold">
+              {#if pauseInput.value}
+                When saved, Tickets will be paused until {new Date(pauseInput.value).toLocaleString(navigator.language, {
+                  dateStyle: "medium",
+                  timeStyle: "long",
+                })}.
+              {:else}
+                {pauseInput.type === "date" ? "Select a date and time." : "Enter a duration."}
+              {/if}
+            </span>
+          </div>
+
+          <div class="flex w-full grow flex-row gap-2">
+            <button
+              class="dy-btn dy-btn-accent grow"
+              disabled={!pauseInput}
+              onclick={() => {
+                pauseInput.value = null;
+                pauseInput.fail = null;
+              }}
+            >
+              Reset
+            </button>
+            <button
+              class="dy-btn dy-btn-primary grow"
+              disabled={!!oldConfig.pausedUntil || pauseInput.fail !== null}
+              onclick={() => {
+                gg.unsavedChanges = true;
+              }}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        {#if pauseInput.fail}
+          <div role="alert" class="dy-alert dy-alert-error dy-alert-soft my-2">
+            <div>
+              <h1>Invalid Input</h1>
+              <p class="text-base-content">{@html pauseInput.fail}</p>
+            </div>
+          </div>
+        {/if}
+      </fieldset>
+    </div>
   </div>
 {:else}
   <span class="dy-loading dy-loading-infinity dy-loading-xl select-none"></span>
