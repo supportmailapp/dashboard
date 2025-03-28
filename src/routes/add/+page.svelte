786abb45d75147e567aaa3eace9a2b4ce3874a23
The-LukeZ <luke.hent3005@gmail.com>
2025-03-28 17:39:58 +0100

    feat(routes): add new page for bot authentication redirection

---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { env } from "$env/dynamic/public";
+  import { urls } from "$lib/constants.js";
+  import { page } from "$app/state";
+
+  onMount(() => {
+    return window.location.assign(urls.botAuth(env.PUBLIC_ClientId, page.params.guildid));
+  });
+</script>
