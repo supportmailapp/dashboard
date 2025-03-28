fb14fd97872c66a20316ce2682ceb57c7d1a404e
The-LukeZ <luke.hent3005@gmail.com>
2025-03-28 17:40:28 +0100

    refactor(routes): update parameter name from slug to guildid in ticket save action

--- src/routes/g/[guildid]/tickets/+page.server.ts
+++ src/routes/g/[guildid]/tickets/+page.server.ts
@@ -1,5 +1,3 @@
-import { updateGuild } from "$lib/server/db";
-
 export const actions = {
-  save: async ({ params: { slug: guildId } }) => {},
+  save: async ({ params: { guildid: guildId } }) => {},
 };
