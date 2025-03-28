bc4a1cca402c0a7f244bfd6df06dff2a95dba83a
The-LukeZ <luke.hent3005@gmail.com>
2025-03-28 17:37:37 +0100

    refactor(cache): streamline guild caching logic and improve type definitions

--- src/lib/cache/guilds.ts
+++ src/lib/cache/guilds.ts
@@ -1,70 +1,94 @@
+import type { RESTAPIPartialCurrentUserGuild } from "discord.js";
 import NodeCache from "node-cache";
 
 const BASE_CACHE_OPTS: NodeCache.Options = {
-  stdTTL: 3600,
+  stdTTL: 1800,
   checkperiod: 60,
   errorOnMissing: false,
 } as const;
 
 const guildsStore = new NodeCache(BASE_CACHE_OPTS);
 
-const guildRolesStore = new NodeCache(BASE_CACHE_OPTS);
+const guildRolesAndChannelsStore = new NodeCache(BASE_CACHE_OPTS);
 
-const userGuildsStore = new NodeCache({ ...BASE_CACHE_OPTS, stdTTL: 1800 });
+const userGuildsStore = new NodeCache(BASE_CACHE_OPTS);
 
-function buildKey(typ: string, guildId: string): `${string}:${string}` {
+function buildKey(typ: "c" | "r", guildId: string): `${string}:${string}` {
   return `${typ}:${guildId}`;
 }
 
-export function parseToCacheGuild(guild: any, configured = false): CachableGuild {
+export function parseToCacheGuild(guild: RESTAPIPartialCurrentUserGuild, configured = false): DCGuild {
   return {
-    id: guild.id,
-    name: guild.name,
-    iconHash: guild.icon || guild.iconHash,
-    isConfigured: guild.isConfigured || configured,
-    permissions: String(guild.permissions),
+    ...guild,
+    isConfigured: configured,
   };
 }
 
 // * Store object data stringified, because Objects have a higher memory footprint and the obejcts are relatively small.
 
-// Setters //
-export function setGuilds(...guilds: CachableGuild[]): void {
+// Guilds functions
+export function cacheGuilds(...guilds: DCGuild[]): void {
   guildsStore.mset(guilds.map((guild) => ({ key: guild.id, val: JSON.stringify(guild) })));
 }
 
-export function overwriteUserGuilds(userId: string, guildIds: string[]): void {
-  userGuildsStore.set(userId, guildIds);
-}
-
-export function setGuildRoles(guildId: string, roles: BasicRole[]): void {
-  guildsStore.set(buildKey("roles", guildId), JSON.stringify(roles));
-}
-
-export function setGuildChannels(guildId: string, channels: BasicChannel[]): void {
-  guildsStore.set(buildKey("channels", guildId), JSON.stringify(channels));
-}
-
-// Getters //
-export function getGuild(id: string): CachableGuild | null {
+export function getDCGuild(id: string): DCGuild | null {
   const guild = guildsStore.get(id) as any;
   if (!guild) return null;
   return JSON.parse(guild);
 }
 
+export function delDCGuild(id: string): void {
+  guildsStore.del(id);
+}
+
+export function clearDCGuilds(): void {
+  guildsStore.flushAll();
+}
+
+// guild roles functions
+export function setGuildRoles(guildId: string, roles: BasicRole[]): void {
+  guildRolesAndChannelsStore.set(buildKey("r", guildId), JSON.stringify(roles));
+}
+
 export function getGuildRoles(guildId: string): BasicRole[] | null {
-  const roles = guildsStore.get(buildKey("roles", guildId)) as any;
+  const roles = guildRolesAndChannelsStore.get(buildKey("r", guildId)) as any;
   if (!roles) return null;
   return JSON.parse(roles);
 }
 
+export function delGuildRoles(guildId: string): void {
+  guildRolesAndChannelsStore.del(buildKey("r", guildId));
+}
+
+export function clearGuildRoles(): void {
+  guildRolesAndChannelsStore.flushAll();
+}
+
+// Guild channels functions
+export function setGuildChannels(guildId: string, channels: BasicChannel[]): void {
+  guildsStore.set(buildKey("c", guildId), JSON.stringify(channels));
+}
+
 export function getGuildChannels(guildId: string): BasicChannel[] | null {
-  const channels = guildsStore.get(buildKey("channels", guildId)) as any;
+  const channels = guildsStore.get(buildKey("c", guildId)) as any;
   if (!channels) return null;
   return JSON.parse(channels);
 }
 
-export function getUserGuilds(userId: string): CachableGuild[] | null {
+export function delGuildChannels(guildId: string): void {
+  guildsStore.del(buildKey("c", guildId));
+}
+
+export function clearGuildChannels(): void {
+  guildsStore.flushAll();
+}
+
+// userGuildsStore functions
+export function overwriteUserGuilds(userId: string, guildIds: string[]): void {
+  userGuildsStore.set(userId, guildIds);
+}
+
+export function getUserGuilds(userId: string): DCGuild[] | null {
   const guildIds = userGuildsStore.get<string[]>(userId);
   if (!guildIds) return null;
   const guilds = guildsStore.mget<string>(guildIds);
@@ -72,43 +96,17 @@ export function getUserGuilds(userId: string): CachableGuild[] | null {
   return Object.values(guilds).map((guild) => JSON.parse(guild));
 }
 
-// Deleters //
-export function delGuild(id: string): void {
-  guildsStore.del(id);
-}
-
-export function delGuildRoles(guildId: string): void {
-  guildsStore.del(buildKey("roles", guildId));
-}
-
-export function delGuildChannels(guildId: string): void {
-  guildsStore.del(buildKey("channels", guildId));
-}
-
 export function delUserGuilds(userId: string): void {
   userGuildsStore.del(userId);
-}
-
-// Clearers //
-export function clearGuilds(): void {
-  guildsStore.flushAll();
-}
-
-export function clearGuildRoles(): void {
-  guildRolesStore.flushAll();
-}
-
-export function clearGuildChannels(): void {
-  guildsStore.flushAll();
 }
 
 export function clearUserGuilds(): void {
   userGuildsStore.flushAll();
 }
 
+// All caches
 export function clearAll(): void {
   guildsStore.flushAll();
-  guildRolesStore.flushAll();
-  guildsStore.flushAll();
+  guildRolesAndChannelsStore.flushAll();
   userGuildsStore.flushAll();
 }
