01cc1f7ca6dd043920cbd7a1c3f4d11850a83b71
The-LukeZ <luke.hent3005@gmail.com>
2025-04-02 17:25:55 +0200

    refactor(navigation): enhance user interface with improved server selection and user avatar display

--- src/lib/components/DesktopNavigation.svelte
+++ src/lib/components/DesktopNavigation.svelte
@@ -5,6 +5,8 @@
   import { ArrowLeftRight, House, MessageSquareWarning, Ticket } from "@lucide/svelte";
   import { slide } from "svelte/transition";
   import { buildNavHref, showServerSelect } from "./navigation.svelte";
+  import Footer from "./Footer.svelte";
+  import { user } from "$lib/stores/user.svelte";
 
   function isCurrentPage(href: string = "/") {
     return page.url.pathname === href;
@@ -33,30 +35,63 @@
 </script>
 
 <nav class="desktop-nav" transition:slide={{ duration: 350, axis: "x" }}>
-  <div class="mb-5 flex h-16 flex-col items-center justify-center px-2">
-    <button class="hover:opacity-80" onclick={showServerSelect} aria-label="Switch Server">
-      <div class="relative size-12">
-        {#if gg.guild}
-          <img
-            src={cdnUrls.guildIcon(gg.guild.id, gg.guild?.icon, 128)}
-            alt="Server icon"
-            class="h-full w-full rounded-full border-0 object-cover"
-          />
-          <ArrowLeftRight class="bg-base-100 absolute -right-0.5 -bottom-0.5 size-5 rounded-full p-0.5 shadow-md" />
-        {:else}
-          <div class="h-full w-full rounded-full bg-slate-800 shadow-md">
-            <div class="dy-skeleton h-full w-full rounded-full"></div>
-          </div>
-        {/if}
-      </div>
-    </button>
-  </div>
-  <div class="flex flex-col items-center gap-2">
-    {#each navItems as item}
-      <a href={item.slug} class="nav-btn h-fit {item.active ? 'active' : ''}">
-        <span class="w-full text-center px-4">{item.label}</span>
-        <item.icon class="size-8" />
+  <!-- Server Changing -->
+  <div class="flex flex-row items-center justify-between gap-5">
+    <div class="dy-tooltip dy-tooltip-bottom dy-tooltip-accent" data-tip="Preferences">
+      <a href="/@me" class="">
+        <div class="relative size-12 cursor-pointer rounded transition-all duration-100 hover:bg-slate-600">
+          {#if user.discord}
+            <img
+              src={cdnUrls.userAvatar(user.discord.id, user.discord.avatar, 128)}
+              alt="User icon"
+              class="h-full w-full rounded-full"
+            />
+          {:else}
+            <div class="h-full w-full rounded-full bg-slate-800 shadow-md">
+              <div class="dy-skeleton h-full w-full rounded-full"></div>
+            </div>
+          {/if}
+        </div>
       </a>
+    </div>
+
+    <div class="dy-tooltip dy-tooltip-bottom dy-tooltip-accent" data-tip="Change Server">
+      <button
+        class="cursor-pointer rounded transition-colors duration-100 hover:bg-slate-600"
+        onclick={showServerSelect}
+        aria-label="Change Server"
+      >
+        <div class="relative size-12">
+          {#if gg.guild?.icon}
+            <img
+              src={cdnUrls.guildIcon(gg.guild.id, gg.guild?.icon, 128)}
+              alt="Server icon"
+              class="h-full w-full rounded-full border-0 object-cover"
+            />
+            <ArrowLeftRight class="bg-base-100 absolute -right-0.5 -bottom-0.5 size-5 rounded-full p-0.5 shadow-md" />
+          {:else}
+            <div class="h-full w-full rounded-full bg-slate-800 shadow-md">
+              <div class="dy-skeleton h-full w-full rounded-full"></div>
+            </div>
+          {/if}
+        </div>
+      </button>
+    </div>
+  </div>
+
+  <!-- Real Navigation -->
+  <ul>
+    {#each navItems as item}
+      <li>
+        <a href={item.slug} class="nav-btn h-fit {item.active ? 'active' : ''}">
+          <span class="w-full px-4 text-center">{item.label}</span>
+          <item.icon class="size-8" />
+        </a>
+      </li>
     {/each}
+  </ul>
+
+  <div class="mt-auto">
+    <Footer />
   </div>
 </nav>
