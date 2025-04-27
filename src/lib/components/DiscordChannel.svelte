9a660dc12e7fd393aef2bf5fce614eb37ce30628
The-LukeZ <luke.hent3005@gmail.com>
2025-04-27 22:35:49 +0200

    feat(DiscordChannel): add channel type handling and replace SVG with icons

--- src/lib/components/DiscordChannel.svelte
+++ src/lib/components/DiscordChannel.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+  import DcThreadIcon from "$lib/assets/DCThreadIcon.svelte";
   import { gg } from "$lib/stores/guild.svelte";
+  import { AlignJustify, Hash } from "@lucide/svelte";
 
   export type ChannelProps = {
     id: string;
     name?: string | undefined;
+    typ?: "thread" | "channel" | "category" | undefined;
   };
 
-  let { id, name = undefined }: ChannelProps = $props();
+  let { id, name = undefined, typ = "channel" }: ChannelProps = $props();
 
   $effect(() => {
     if (!name) {
@@ -22,8 +25,12 @@
 
 <!-- svelte-ignore a11y_no_static_element_interactions, a11y_click_events_have_key_events -->
 <div class="discord-channel" onclickcapture={clickFunction}>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
-  </svg>
+  {#if typ === "thread"}
+    <DcThreadIcon />
+  {:else if typ === "category"}
+    <AlignJustify />
+  {:else}
+    <Hash />
+  {/if}
   <span>{String(name)}</span>
 </div>
