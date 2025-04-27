e756a0d9f58bd3e833d3f458105c33daafa98341
The-LukeZ <luke.hent3005@gmail.com>
2025-04-27 22:40:44 +0200

    refactor(news): clear initial news data array for cleaner state management

--- src/lib/utils/news.ts
+++ src/lib/utils/news.ts
@@ -1,30 +1,4 @@
-const news: News[] = [
-  {
-    id: "topic_1",
-    type: "info",
-    title: "General Info",
-    content: "Content 1",
-  },
-  {
-    id: "topic_2",
-    type: "success",
-    title: "Welcome to the new dashboard!",
-    content:
-      "We've updated the dashboard to be more user-friendly and easier to navigate. If you have any feedback, please let us know!",
-  },
-  {
-    id: "topic_3",
-    type: "warning",
-    title: "Warning Stuff",
-    content: "Content 3",
-  },
-  {
-    id: "topic_4",
-    type: "error",
-    title: "Error Stuff",
-    content: "Content 4",
-  },
-];
+const news: News[] = [];
 
 export const getNews = () => {
   return news;
