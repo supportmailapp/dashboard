48eafcd29e31469cfb2598fb3f1249ccb7984078
The-LukeZ <luke.hent3005@gmail.com>
2025-04-27 22:34:06 +0200

    refactor(lib): implement function for converting guild configuration overview

---
+++ src/lib/index.ts
@@ -0,0 +1,17 @@
+import type { IDBGuild } from "supportmail-types";
+
+export function turnGuildConfigIntoOverview(_config: IDBGuild & { _id: string }) {
+  return {
+    _id: _config._id,
+    id: _config.id,
+    name: _config.name,
+    icon: _config.icon,
+    lang: _config.lang,
+    ticketForum: _config.ticketConfig.forumId,
+    alertChannel: _config.reportConfig.channelId,
+  };
+}
+
+export type ConfigOverview = ReturnType<typeof turnGuildConfigIntoOverview>;
+
+export * from "./utils/validators";
