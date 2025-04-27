95ea0d130334f13943717a44045e2bd481b4bd8e
The-LukeZ <luke.hent3005@gmail.com>
2025-04-27 22:42:48 +0200

    refactor(tickets): simplify saving state handling and improve subscription logic

--- src/routes/g/[guildid]/tickets/+page.svelte
+++ src/routes/g/[guildid]/tickets/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
   import { saving } from "$lib/stores/config.svelte";
 
-  $effect(async () => {
-    if ($saving) {
-      // Perform actions when saving is true
-      console.log("Saving state is true");
-    } else {
-      // Perform actions when saving is false
-      console.log("Saving state is false");
-    }
-  });
-
-  saving.subscribe(async (value) => {
-    if (value) {
+  $effect(() => {
+    if (saving.value) {
       // Perform actions when saving is true
       console.log("Saving state is true");
     } else {
