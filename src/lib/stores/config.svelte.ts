03a27e17c68eb4a30cff5da621ef0a1dd8b052a9
The-LukeZ <luke.hent3005@gmail.com>
2025-04-27 22:40:23 +0200

    refactor(config): simplify config store and remove unused functions
    Now only store the oldConfig globally (so we don't have to do this in every file manually since we don't need type-safety for that !here!)

--- src/lib/stores/config.svelte.ts
+++ src/lib/stores/config.svelte.ts
@@ -1,39 +1,21 @@
-import { BASIC_GET_FETCH_INIT } from "$lib/constants";
-import type { IDBGuild } from "supportmail-types";
 import { writable } from "svelte/store";
 
 /**
- * This can be used to store ANY config data. It is not type-safe and should be used with caution.
- *
- * It's recommended to use a derived state on each page for type safety.
+ * Just a simple store to hold the old configuration to allow for
+ * reverting changes.
  */
-export const configStore = writable<any | null>(null);
+export let oldConfig = writable<unknown>(null);
 
 export let unsavedChanges = writable<boolean>(false);
-export let saving = writable<boolean>(false);
-export let error = writable<string | null>(null);
-
-error.subscribe((value) => {
-  console.log("Error:", value);
+export let saving = $state<{ value: boolean; progress: number | null }>({
+  value: false,
+  progress: null,
 });
+export let resetConfig = writable<boolean>(false);
+export let configError = writable<{ note: string; objs: unknown[] } | null>(null);
 
-/**
- * Loads config data from a specified endpoint and sets it in the config store.
- * @param endpoint The endpoint to fetch the config from
- * @param fetchOptions The fetch options to use. Default is a basic GET request.
- */
-export async function loadConfig(endpoint: string, fetchOptions: RequestInit = BASIC_GET_FETCH_INIT) {
-  const fetchOpts = Object.assign({}, BASIC_GET_FETCH_INIT, fetchOptions);
-  console.log(`Fetching config from ${endpoint} with options:`, fetchOpts);
-  const configRes = await fetch(endpoint, fetchOpts);
-  console.log(`Response status: ${configRes.status}`);
-
-  if (configRes.ok) {
-    configStore.set((await configRes.json()) as IDBGuild);
-  } else {
-    // throw new Error("Failed to fetch guild config", {
-    //   cause: configRes,
-    // });
-    error.set(configRes.statusText);
+configError.subscribe((value) => {
+  if (value) {
+    console.error(value);
   }
-}
+});
