5b8ac484eeac4289b4faea12d2a1a6f0feb6bc66
The-LukeZ <luke.hent3005@gmail.com>
2025-04-30 15:09:22 +0200

    feat(RoleSelector): enhance role selection with search functionality and improved UI

--- src/lib/components/RoleSelector.svelte
+++ src/lib/components/RoleSelector.svelte
@@ -1,42 +1,83 @@
 <script lang="ts">
   import { gg } from "$lib/stores/guild.svelte";
-  import { Search } from "@lucide/svelte";
+  import { numberToHex } from "$lib/utils/formatting";
+  import { Circle } from "@lucide/svelte";
+  import { blur, scale } from "svelte/transition";
 
   type Props = {
+    /**
+     * An array of role IDs to exclude from the selection.
+     */
+    exclude?: string[];
+    /**
+     * Whether to exclude managed roles from the list.
+     */
+    managedRolesBehavior?: "exclude" | "include" | "no-select";
+    /**
+     * Whether to exclude the `@everyone` role from the list.
+     */
+    excludeAtEveryone?: boolean;
+    /**
+     * The HTML element to anchor the role selector to.
+     */
+    anchor?: HTMLElement;
     onselect?: (role: BasicRole) => void;
   };
 
-  let { onselect = () => {} }: Props = $props();
+  let {
+    exclude = [],
+    managedRolesBehavior = "no-select",
+    excludeAtEveryone = false,
+    onselect = () => {
+      console.warn("No handler defined!");
+    },
+  }: Props = $props();
+
+  function roleIsAllowed(role: BasicRole) {
+    if (exclude.includes(role.id)) return false;
+    if (managedRolesBehavior === "exclude" && role.managed) return false;
+    if (excludeAtEveryone && role.id === gg.guild?.id) return false;
+    return true;
+  }
 
   let selected = $state<BasicRole | undefined>(undefined);
   let search = $state("");
+  let filteredRoles = $derived<BasicRole[]>(gg.roles?.filter((r) => roleIsAllowed(r)) || []);
 </script>
 
-<!-- TODO: How to anchor this to a parent HTML element? like, centered/left/right - top/bottom -->
-<div class="flex flex-col">
-  <label class="dy-input">
-    <Search class="size-5" />
-    <input type="search" bind:value={search} placeholder="Role..." class="grow" />
+<div class="flex h-64 w-52 flex-col gap-1 p-2" transition:scale={{ duration: 150 }}>
+  <label class="dy-input h-8">
+    <input type="search" bind:value={search} placeholder="Role..." class="h-8" />
   </label>
-  {#if gg.roles}
-    <ul class="dy-menu h-64 w-52">
-      {#each gg.roles as role}
-        {#if search == "" || role.name.toLowerCase().includes(search.toLowerCase())}
-          <li>
-            <button
-              role="option"
-              aria-selected={selected == role}
-              onclick={() => {
-                selected = role;
-                onselect(role);
-              }}
-            >
-              {role.name}
-            </button>
-          </li>
+  {#if filteredRoles.length}
+    <div
+      class="bg-base-200 grid grid-cols-1 place-items-start gap-1 overflow-x-hidden overflow-y-auto rounded border border-slate-500 p-2"
+    >
+      {#if search !== "" && filteredRoles.filter((role) => role.name.toLowerCase().includes(search.toLowerCase())).length === 0}
+        <div class="dy-list-item">No roles found</div>
+      {/if}
+      {#each filteredRoles as role}
+        {#if search === "" || role.name.toLowerCase().includes(search.toLowerCase())}
+          <button
+            role="gridcell"
+            class="flex w-full items-center gap-2 rounded p-2 transition-all duration-100 hover:bg-slate-500/70 {managedRolesBehavior ===
+              'no-select' && role.managed
+              ? 'cursor-not-allowed opacity-40'
+              : ''}"
+            aria-selected={selected == role}
+            onclick={() => {
+              selected = role;
+              onselect(role);
+            }}
+            transition:blur={{ duration: 100 }}
+            disabled={managedRolesBehavior === "no-select" && role.managed}
+          >
+            <Circle class="size-3" fill={"#" + numberToHex(role.color)} color="none" />
+            <span class="truncate">{role.name}</span>
+          </button>
         {/if}
       {/each}
-    </ul>
+    </div>
   {:else}
     <div class="dy-list-item">No roles found</div>
   {/if}
