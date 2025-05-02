3d9418744c184d544b00f6db690aab000c249768
The-LukeZ <luke.hent3005@gmail.com>
2025-05-03 00:47:29 +0200

    feat(SiteHeader): enhance layout with breadcrumbs support

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-  let { children } = $props();
+  import type { Snippet } from "svelte";
+
+  type Props = {
+    breadcrumbs?: {
+      title: string;
+      href: string;
+    }[];
+    /**
+     * Just the Title of the current site.
+     */
+    children: Snippet;
+  };
+  let { children, breadcrumbs = [] }: Props = $props();
 </script>
 
-<h1 class="text-3xl font-bold">
-  {@render children()}
-</h1>
+<div class="dy-breadcrumbs text-3xl font-bold">
+  <ul>
+    {#if breadcrumbs.length > 0}
+      {#each breadcrumbs as crumb}
+        <li>
+          <a href={crumb.href} class="text-gray-500 hover:text-gray-700">
+            {crumb.title}
+          </a>
+        </li>
+      {/each}
+    {/if}
+    <li>{@render children()}</li>
+  </ul>
+</div>
