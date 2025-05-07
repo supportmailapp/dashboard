077d2b293e974e24533e0777b0e66c428f36ac30
The-LukeZ <luke.hent3005@gmail.com>
2025-05-07 22:14:34 +0200

    fix(ServerSelectDialog): update transition effects and improve styling

--- src/lib/components/ServerSelectDialog.svelte
+++ src/lib/components/ServerSelectDialog.svelte
@@ -2,7 +2,7 @@
   import { page } from "$app/state";
   import { guilds } from "$lib/stores/guilds.svelte";
   import { cdnUrls } from "$lib/utils/formatting";
-  import { slide } from "svelte/transition";
+  import { scale, slide } from "svelte/transition";
 
   let innerWidth = $state(800);
 </script>
@@ -10,12 +10,12 @@
 <svelte:window bind:innerWidth />
 
 <dialog id="server-select" class="dy-modal dy-modal-bottom md:dy-modal-middle overflow-hidden">
-  <div class="dy-modal-box bg-base-200 h-screen max-h-screen md:h-fit" transition:slide={{ duration: 200, axis: "y" }}>
-    <div class="mb-3 flex flex-row">
+  <div class="dy-modal-box bg-base-200 max-h-screen md:max-h-fit" transition:scale={{ duration: 200 }}>
+    <div class="mb-3 flex flex-row overflow-hidden">
       <h3 class="dy-modal-title font-semibold select-none">Select a Server</h3>
       <form method="dialog" class="ml-auto">
         <button
-          class="dy-btn dy-btn-circle dy-btn-lg flex-row"
+          class="dy-btn dy-btn-circle dy-btn-sm flex-row"
           aria-label="Close"
           onclick={() => (document.getElementById("server-select") as HTMLDialogElement).close()}
         >
@@ -23,13 +23,14 @@
         </button>
       </form>
     </div>
+
     <div class="max-h-full overflow-y-auto md:max-h-full">
-      <ul class="dy-dropdown-content dy-menu bg-base-100 rounded-box dy z-1 w-full gap-y-1 overflow-y-auto p-2 shadow-sm">
+      <ul class="dy-dropdown-content dy-menu bg-base-300 rounded-box dy z-1 w-full gap-y-1 p-2 shadow-sm">
         {#each guilds.value as guild}
           <li>
             {#if page.url.pathname.startsWith("/g/" + guild.id)}
               <!-- svelte-ignore a11y_missing_attribute -->
-              <a class="bg-base-300 cursor-default">
+              <a class="bg-base-content/10 cursor-default">
                 <div class="dy-avatar">
                   <div class="dy-mask dy-mask-squircle size-8">
                     <img src={cdnUrls.guildIcon(guild.id, guild.icon, "512")} alt="Guild Icon" />
@@ -38,7 +39,7 @@
                 <span class="truncate">{guild.name}</span>
               </a>
             {:else}
-              <a href="/{!guild.isConfigured ? 'add/' : ''}{guild.id}" target="_self">
+              <a href="{!guild.isConfigured ? '/add' : '/g'}/{guild.id}" target="_self">
                 <div class="dy-avatar">
                   <div class="dy-mask dy-mask-squircle size-8">
                     <img src={cdnUrls.guildIcon(guild.id, guild.icon, "512")} alt="Guild Icon" />
