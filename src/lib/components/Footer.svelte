6b38c77d173fcee43b2f99abf8fee44826e7ea42
The-LukeZ <luke.hent3005@gmail.com>
2025-05-07 22:14:29 +0200

    fix(footer): add server-select class based on current page URL

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
   import { ImportantLinks } from "$lib/urls";
 </script>
 
-<footer class="footer-container">
+<footer class="footer-container" class:server-select={page.url.pathname === "/"}>
   <a href={ImportantLinks.legal.notice} target="_blank">SupportMail © {page.data.ccDate}</a>
   <a href={ImportantLinks.legal.terms} target="_blank">Terms of Service</a>
   <a href={ImportantLinks.legal.privacy} target="_blank">Privacy Policy</a>
