0dce4146d08221834d42f6ce5601c2262324f0da
The-LukeZ <luke.hent3005@gmail.com>
2025-05-12 01:07:10 +0200

    refactor(user-store): remove db property and loadDbUser function from UserState

--- src/lib/stores/user.svelte.ts
+++ src/lib/stores/user.svelte.ts
@@ -1,20 +1,5 @@
-import { APIRoutes } from "$lib/urls";
-import { BASIC_GET_FETCH_INIT } from "$lib/constants";
-import type { IDBUser } from "supportmail-types";
-
 type UserState = {
   discord: BasicUser | null;
-  db: IDBUser | null;
 };
 
-export let user = $state<UserState>({ discord: null, db: null });
-
-export async function loadDbUser() {
-  const res = await fetch(APIRoutes.user(), BASIC_GET_FETCH_INIT);
-
-  if (res.ok) {
-    user.db = (await res.json()) as IDBUser;
-  } else {
-    console.error("Failed to load user data", res);
-  }
-}
+export let user = $state<UserState>({ discord: null });
