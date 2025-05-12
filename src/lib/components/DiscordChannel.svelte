71d2cc67d86c0c50b203188a34c2c81905b62008
The-LukeZ <luke.hent3005@gmail.com>
2025-05-12 03:56:22 +0200

    feat(discord-channel): add discordType prop and update typ based on discordChannelTypeToBasic

--- src/lib/components/DiscordChannel.svelte
+++ src/lib/components/DiscordChannel.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+  import { discordChannelTypeToBasic } from "$lib";
   import DcThreadIcon from "$lib/assets/DCThreadIcon.svelte";
   import { gg } from "$lib/stores/guild.svelte";
   import { AlignJustify, Hash } from "@lucide/svelte";
+  import type { ChannelType } from "discord.js";
 
-  export type ChannelProps = {
+  type ChannelProps = {
     id: string;
     name?: string | undefined;
     typ?: "thread" | "channel" | "category";
+    discordType?: ChannelType;
     /**
      * If true, the channel name will be cut off at 10rem (160px) and will show an ellipsis.
      */
     cutLength?: boolean;
   };
 
-  let { id, name = undefined, typ = "channel", cutLength = false }: ChannelProps = $props();
+  let { id, name = undefined, typ = "channel", discordType = undefined, cutLength = false }: ChannelProps = $props();
   const channelClasses = {
     category: "category",
     thread: "", // Uses default channel class
@@ -23,6 +26,12 @@
   $effect(() => {
     if (!name) {
       name = gg.channels?.find((c) => c.id == id)?.name;
+    }
+  });
+
+  $effect(() => {
+    if (discordType) {
+      typ = discordChannelTypeToBasic(discordType);
     }
   });
 </script>
