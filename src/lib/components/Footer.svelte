dfa89cbe0bc2484ea7839a32ef8b5a2f056f75c3
The-LukeZ <luke.hent3005@gmail.com>
2025-05-12 16:19:34 +0200

    feat(footer): add conditional margin class based on page pathname

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
   import { ImportantLinks } from "$lib/urls";
 </script>
 
-<footer class="footer-container">
+<footer class="footer-container" class:ml-0={page.url.pathname === "/" || page.url.pathname === "/@me"}>
   <a href={ImportantLinks.legal.notice} target="_blank">SupportMail © {page.data.ccDate}</a>
   <a href={ImportantLinks.legal.terms} target="_blank">Terms of Service</a>
   <a href={ImportantLinks.legal.privacy} target="_blank">Privacy Policy</a>
