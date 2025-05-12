3e31069dc94e3ec79774a476412501ec51c3adbb
The-LukeZ <luke.hent3005@gmail.com>
2025-05-12 16:19:29 +0200

    feat(discord-channel): add voice type support and enhance click functionality

--- src/lib/components/DiscordChannel.svelte
+++ src/lib/components/DiscordChannel.svelte
@@ -2,25 +2,40 @@
   import { discordChannelTypeToBasic } from "$lib";
   import DcThreadIcon from "$lib/assets/DCThreadIcon.svelte";
   import { gg } from "$lib/stores/guild.svelte";
-  import { AlignJustify, Hash } from "@lucide/svelte";
+  import { AlignJustify, Hash, Volume2 } from "@lucide/svelte";
   import type { ChannelType } from "discord.js";
 
   type ChannelProps = {
     id: string;
     name?: string | undefined;
-    typ?: "thread" | "channel" | "category";
+    typ?: "thread" | "channel" | "category" | "voice";
     discordType?: ChannelType;
     /**
      * If true, the channel name will be cut off at 10rem (160px) and will show an ellipsis.
      */
     cutLength?: boolean;
+    clickFn?:
+      | undefined
+      | ((
+          e: MouseEvent & {
+            currentTarget: EventTarget & HTMLDivElement;
+          },
+        ) => void);
   };
 
-  let { id, name = undefined, typ = "channel", discordType = undefined, cutLength = false }: ChannelProps = $props();
+  let {
+    id,
+    name = undefined,
+    typ = "channel",
+    discordType = undefined,
+    cutLength = false,
+    clickFn = undefined,
+  }: ChannelProps = $props();
   const channelClasses = {
     category: "category",
     thread: "", // Uses default channel class
-    channel: "", // Uses default channel class
+    channel: "",
+    voice: "",
   };
 
   $effect(() => {
@@ -38,15 +53,21 @@
 
 <div
   class="discord-mention {channelClasses[typ]}"
-  onclickcapture={function clickFunction(_: any) {
+  onclickcapture={(e) => {
+    if (clickFn !== undefined) {
+      return clickFn(e);
+    }
     navigator.clipboard.writeText(id);
     alert(`Copied ${typ} ID to clipboard!`);
+    e.stopPropagation();
   }}
 >
   {#if typ === "thread"}
     <DcThreadIcon />
   {:else if typ === "category"}
     <AlignJustify />
+  {:else if typ === "voice"}
+    <Volume2 />
   {:else}
     <Hash />
   {/if}
