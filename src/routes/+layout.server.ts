9af85cdb9fe48ef405c418ce68ab471b5b386989
The-LukeZ <luke.hent3005@gmail.com>
2025-05-15 00:24:33 +0200

    feat(layout): add URL handling and redirect logic for refresh token

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,13 @@
-export const load = async ({ locals }) => {
+import { APIRoutes } from "$lib/urls.js";
+import { redirect } from "@sveltejs/kit";
+
+export const load = async ({ locals, url }) => {
+  // Redirect to refresh token endpoint if the user is not logged in
+  // and the refresh token is present
+  if (locals.refreshToken) {
+    redirect(302, APIRoutes.refresh(url.pathname));
+  }
+
   return {
     ccDate: "2025",
     user: locals.user,
