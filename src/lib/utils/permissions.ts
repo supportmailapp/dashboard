a322108f58b43c06e3259f884cf5aaf9f8ae283d
The-LukeZ <luke.hent3005@gmail.com>
2025-06-11 01:26:34 +0200

    feat(permissions): add utility functions for checking Discord permissions

---
+++ src/lib/utils/permissions.ts
@@ -0,0 +1,42 @@
+// Permission utilities for Discord permissions.
+
+import { PermissionsBitField, type PermissionsString } from "discord.js";
+
+type PermissionResolvable = PermissionsString | bigint;
+
+/**
+ * Checks if a specific permission exists in a permission set.
+ * @param permissions - The permissions to check.
+ * @param permission - The permission to check for.
+ *
+ * ### Note: If the user has admin, `true` will be returned regardless of the other permission.
+ */
+export function hasPermission(permissions: PermissionResolvable, permission: PermissionsString) {
+  return new PermissionsBitField(permissions).has(permission);
+}
+
+/**
+ * Checks if the given permissions include all the specified permission flags.
+ *
+ * @param permissions - The set of permissions to check against.
+ * @param permissionFlags - The permission flags to check for.
+ * @returns `true` if all the specified permission flags are included in the given permissions, otherwise `false`.
+ *
+ * ### Note: If the user has admin, `true` will be returned regardless of the other permissions.
+ */
+export function hasPermissions(permissions: PermissionResolvable, ...permissionFlags: PermissionResolvable[]): boolean {
+  return permissionFlags.every((permissionFlag) => new PermissionsBitField(permissions).has(permissionFlag));
+}
+
+/**
+ * Checks if the given permissions include the ability to manage the bot.
+ *
+ * This function verifies if the provided `permissions` contain both
+ * "Administrator" and "ManageGuild" permissions.
+ *
+ * @param permissions - The permission bits to check.
+ * @returns `true` if the permissions include both "administrator" and "manageGuild", otherwise `false`.
+ */
+export function canManageBot(permissions: bigint): boolean {
+  return hasPermissions(permissions, "Administrator", "ManageGuild");
+}
