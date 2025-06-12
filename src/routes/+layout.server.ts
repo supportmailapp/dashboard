2cc647102260554ca8192ed770b30666ccef2fb9
The-LukeZ <luke.hent3005@gmail.com>
2025-06-12 11:12:17 +0200

    feat(routes): log loading process and user information in load function

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,9 @@
 export const prerender = false;
 
 export async function load({ locals }) {
+  console.log("Loading +layout.server.ts");
+  console.log("User:", locals.user);
   return {
     user: locals.user,
-    session: locals.session,
   };
 }
