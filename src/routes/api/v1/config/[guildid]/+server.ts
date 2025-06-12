5ede95d8edbae2f935162b19242cbd71f38ac4f6
The-LukeZ <luke.hent3005@gmail.com>
2025-06-12 16:36:52 +0200

    refactor(api): streamline GET functions and remove unused routes for guilds and tickets

--- src/routes/api/v1/config/[guildid]/+server.ts
+++ src/routes/api/v1/config/[guildid]/+server.ts
@@ -1,13 +1,11 @@
-import { SchemaValidator, type ConfigOverview } from "$lib";
-import { SupportedLanguages } from "$lib/constants.js";
-import { getGuild, updateGuild } from "$lib/server/db";
+import { getDBGuild } from "$lib/server/db";
 import type { IDBGuild } from "supportmail-types";
 
-export const GET = async ({ locals }) => {
+export async function GET({ locals }) {
   let config: IDBGuild | any | null = null;
   if (locals.guildId && locals.token) {
     if (!config) {
-      const guild = await getGuild(locals.guildId);
+      const guild = await getDBGuild(locals.guildId);
       if (!guild) {
         return Response.json("Not Found", { status: 404, statusText: "Not Found" });
       }
@@ -19,32 +17,4 @@ export const GET = async ({ locals }) => {
   }
 
   return Response.json("Bad Request", { status: 400, statusText: "Bad Request" });
-};
-
-const baseConfigSchema = new SchemaValidator<Omit<ConfigOverview, "_id" | "ticketForum" | "alertChannel">>({
-  id: { type: "string", required: true },
-  icon: { type: ["string", "null"], required: true },
-  name: { type: "string", required: true },
-  lang: { type: "string", required: true, enum: SupportedLanguages.map((lang) => lang.value) },
-});
-
-export const PATCH = async ({ locals, request }) => {
-  if (locals.guildId && locals.token) {
-    const update = (await request.json()) as Record<string, any>;
-    const result = baseConfigSchema.validate(update);
-    if (!result.isValid || Object.entries(result.errors).length > 0) {
-      console.debug("Validation errors", result.errors);
-      return Response.json(result, { status: 400, statusText: "Bad Request" });
-    }
-
-    await updateGuild(locals.guildId, {
-      $set: {
-        ...result.value,
-      },
-    });
-
-    return new Response(null, { status: 204, statusText: "No Content" });
-  }
-
-  return Response.json("Bad Request", { status: 400, statusText: "Bad Request" });
-};
+}
