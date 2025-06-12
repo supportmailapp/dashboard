5c4c97c3aebfbe288040ad74583c2b37434cd0fe
The-LukeZ <luke.hent3005@gmail.com>
2025-06-12 12:01:15 +0200

    feat(api): enhance GET guilds endpoint with error handling and bot configuration check

--- src/routes/api/v1/@me/guilds/+server.ts
+++ src/routes/api/v1/@me/guilds/+server.ts
@@ -1,13 +1,40 @@
 import { JsonErrors } from "$lib/constants";
+import { DBGuild } from "$lib/server/db";
 import { DiscordUserAPI } from "$lib/server/discord";
+import { canManageBot } from "$lib/utils/permissions";
 import { type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ locals, url }) => {
   if (!(locals.user?.id && locals.token)) return JsonErrors.unauthorized();
 
-  const rest = locals.discordUserRest || new DiscordUserAPI(locals.token.accessToken);
+  const rest = locals.discordUserRest || new DiscordUserAPI(locals.token.accessToken, locals.user.id);
 
-  let guilds = await rest.getCurrentUserGuilds(locals.user.id);
+  const guildsRes = await rest.getCurrentUserGuilds(locals.user.id);
+  if (!guildsRes.isSuccess()) {
+    let errorText = "Server Error";
+    if (guildsRes.hasDiscordAPIError()) {
+    }
+  }
 
-  return Response.json(guilds, { status: 200, statusText: "OK" });
+  const rawGuilds = guildsRes.data!;
+  const allGuildIds = rawGuilds.map((g) => g.id);
+
+  // Find all guilds where
+  const dbGuilds = await DBGuild.find({ id: { $in: allGuildIds } }, { id: true }, { lean: true });
+  const dbGuildIds = dbGuilds.map((dbg) => dbg.id);
+
+  const botGuilds: BotGuild[] = rawGuilds.map((rg) => ({
+    ...rg,
+    isConfigured: dbGuildIds.includes(rg.id as any),
+  }));
+
+  const filterParams = {
+    manageBotOnly: url.searchParams.get("manageBot") === "true",
+  };
+
+  if (filterParams.manageBotOnly) {
+    return Response.json(botGuilds.filter((g) => canManageBot(g.permissions)));
+  }
+
+  return Response.json(botGuilds);
 };
