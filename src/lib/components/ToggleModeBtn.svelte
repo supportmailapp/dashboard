644d55062881b27e8fb120843a665ede2fba62b5
The-LukeZ <luke.hent3005@gmail.com>
2025-06-12 23:11:14 +0200

    refactor(toggle-mode-btn): replace BlendIcon with SunIcon and MoonIcon for theme toggle

--- src/lib/components/ToggleModeBtn.svelte
+++ src/lib/components/ToggleModeBtn.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-  import BlendIcon from "@lucide/svelte/icons/blend";
+  import SunIcon from "@lucide/svelte/icons/sun";
+  import MoonIcon from "@lucide/svelte/icons/moon";
 
-  import { toggleMode, mode } from "mode-watcher";
+  import { toggleMode } from "mode-watcher";
   import { Button } from "$lib/components/ui/button/index.js";
-  import { cn } from "$lib/utils";
-
-  let isDark = $derived(mode.current?.toString() === "dark");
 </script>
 
 <Button onclick={toggleMode} variant="outline" size="icon">
-  <BlendIcon
-    class={cn(
-      "size-[1.2rem] transition-transform duration-500 ease-in-out",
-      isDark ? "rotate-90" : "rotate-0",
-    )}
+  <SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+  <MoonIcon
+    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
   />
   <span class="sr-only">Toggle theme</span>
 </Button>
