96ac1d3020ca4dd8574467a5d7bb25867e09457c
The-LukeZ <luke.hent3005@gmail.com>
2025-06-12 23:17:12 +0200

    fix(toggle-mode-btn): update theme toggle functionality and improve icon animations

--- src/lib/components/ToggleModeBtn.svelte
+++ src/lib/components/ToggleModeBtn.svelte
@@ -2,14 +2,27 @@
   import SunIcon from "@lucide/svelte/icons/sun";
   import MoonIcon from "@lucide/svelte/icons/moon";
 
-  import { toggleMode } from "mode-watcher";
+  import { mode, setMode } from "mode-watcher";
   import { Button } from "$lib/components/ui/button/index.js";
+  import { cn } from "$lib/utils";
+
+  let isDark = $derived(mode.current === "dark");
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
-  <SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+<!-- TODO: Why the heck is the animation not working?? -->
+
+<Button onclick={() => setMode(isDark ? "light" : "dark")} variant="outline" size="icon">
+  <SunIcon
+    class={cn(
+      "h-[1.2rem] w-[1.2rem] transition-transform duration-150",
+      !isDark ? "scale-100 rotate-0" : "scale-0 rotate-90",
+    )}
+  />
   <MoonIcon
-    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+    class={cn(
+      "absolute h-[1.2rem] w-[1.2rem] transition-transform duration-150",
+      isDark ? "scale-100 rotate-0" : "scale-0 rotate-90",
+    )}
   />
   <span class="sr-only">Toggle theme</span>
 </Button>
