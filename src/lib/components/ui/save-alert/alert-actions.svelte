df961d7da772675d42356d1c848aebb02d5419f5
The-LukeZ <luke.hent3005@gmail.com>
2025-06-16 15:10:08 +0200

    feat(save-alert): implement alert component structure with actions, content, description, and title

---
+++ src/lib/components/ui/save-alert/alert-actions.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { cn, type WithElementRef } from "$lib/utils.js";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="alert-actions"
+  class={cn("inline-flex w-full sm:w-fit justify-center items-center gap-2 h-full", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
---
+++ src/lib/components/ui/save-alert/alert-content.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { cn, type WithElementRef } from "$lib/utils.js";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="alert-content"
+  class={cn("flex flex-1 flex-col text-center gap-1 sm:text-start", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
