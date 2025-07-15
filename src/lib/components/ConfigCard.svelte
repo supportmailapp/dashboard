c64918d6287f38e1c32f6af154c65585b265265d
The-LukeZ <luke.hent3005@gmail.com>
2025-07-15 22:57:50 +0200

    feat(ConfigCard): add new component for configuration card with save functionality

---
+++ src/lib/components/ConfigCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import * as Card from "$ui/card/index.js";
+  import { Button } from "$ui/button";
+
+  type Props = {
+    title: string;
+    description?: string;
+    saveFn: () => void | Promise<void>;
+    children?: Snippet;
+  };
+  let { children, saveFn, title, description }: Props = $props();
+</script>
+
+<Card.Root>
+  <Card.Header>
+    <Card.Title>{title}</Card.Title>
+    {#if description}
+      <Card.Description>{description}</Card.Description>
+    {/if}
+  </Card.Header>
+  <Card.Content>
+    {@render children?.()}
+  </Card.Content>
+  <Card.Footer class="justify-end">
+    <Button variant="default" onclick={saveFn}>Save</Button>
+  </Card.Footer>
+</Card.Root>
