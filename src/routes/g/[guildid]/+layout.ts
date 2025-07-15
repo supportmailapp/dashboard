f6a31c39d3b309cbb42776b0a1e7c7a151eff95a
The-LukeZ <luke.hent3005@gmail.com>
2025-07-15 15:52:37 +0200

    refactor(layout): remove DataManager instantiation from load function

--- src/routes/g/[guildid]/+layout.ts
+++ src/routes/g/[guildid]/+layout.ts
@@ -1,11 +1,9 @@
 import { page } from "$app/state";
 import { isCurrentPage as _isCurrentPage, guildHref as _guildHref } from "$lib";
-import { DataManager } from "$lib/stores/DataManager.svelte.js";
 
 export async function load({ params, parent }) {
   await parent();
   return {
-    manager: new DataManager(),
     guildId: params.guildid,
     isCurrentPage: (href: string, partial: boolean = false) => _isCurrentPage(page.url, href, partial),
     guildHref: (path: string) => _guildHref(params.guildid, path),
