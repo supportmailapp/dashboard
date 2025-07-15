a8f6fdbc1bc648b1612cca7f84b1df521580135b
The-LukeZ <luke.hent3005@gmail.com>
2025-07-15 22:35:45 +0200

    feat(pausing): implement PUT endpoint for pausing configuration with validation

---
+++ src/routes/api/v1/guilds/[guildid]/config/pausing/+server.ts
@@ -0,0 +1,13 @@
+import { JsonErrors } from "$lib/constants";
+import { getDBGuild } from "$lib/server/db/utils.js";
+
+export async function GET({ locals }) {
+  if (!locals.token && !locals.user) return JsonErrors.badRequest();
+
+  const pausing = await getDBGuild(locals.guildId!, "pausing");
+
+  if (!pausing) {
+    return JsonErrors.notFound();
+  }
+  return Response.json(pausing);
+}
