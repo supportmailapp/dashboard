f8ffddc4b75cc2e80a3bec31153a38c9bed946c4
The-LukeZ <luke.hent3005@gmail.com>
2025-07-16 17:13:51 +0200

    feat(PUT): enhance date validation logic and improve error handling

--- src/routes/api/v1/guilds/[guildid]/config/pausing/[modul]/+server.ts
+++ src/routes/api/v1/guilds/[guildid]/config/pausing/[modul]/+server.ts
@@ -1,6 +1,7 @@
 import { JsonErrors } from "$lib/constants";
 import { FlattenDocToJSON, updateDBGuild } from "$lib/server/db/utils.js";
 import { MyValidator } from "$lib/server/validators/index.js";
+import dayjs from "dayjs";
 import z from "zod";
 
 const putSchema = z.object({
@@ -18,13 +19,24 @@ export async function PUT({ locals, params, request }) {
   }
 
   const body = await request.json();
+  console.log("body", body);
   if (!body) {
     return JsonErrors.badRequest("Missing body?");
   }
 
   const valRes = new MyValidator(putSchema).validate(body);
+  console.log("valRes", valRes);
   if (!valRes.success) {
+    console.log("no success", valRes.error);
     return JsonErrors.badRequest(valRes.error.message);
+  }
+  console.log("success", valRes.data);
+
+  const pauseDjs = valRes.data.date ? dayjs(valRes.data.date) : null;
+  if (pauseDjs && !pauseDjs.isValid()) {
+    return JsonErrors.badRequest("Invalid Date.");
+  } else if (pauseDjs?.isBefore(new Date())) {
+    return JsonErrors.badRequest("Invalid Date. Must be in the future!");
   }
 
   const configKey = params.modul === "tickets" ? "ticketConfig" : "reportConfig";
@@ -33,7 +45,7 @@ export async function PUT({ locals, params, request }) {
     $set: {
       [`${configKey}.pausedUntil`]: {
         value: valRes.data.value,
-        date: valRes.data.date,
+        date: pauseDjs?.toDate() ?? null,
       },
     },
   });
@@ -44,6 +56,17 @@ export async function PUT({ locals, params, request }) {
 
   const flat = FlattenDocToJSON(newDBGuild)[configKey]["pausedUntil"];
   console.log("flat", flat);
+
+  console.log("1", new MyValidator(putSchema).validate({}));
+  console.log("2", new MyValidator(putSchema).validate("asasd"));
+  console.log("3", new MyValidator(putSchema).validate(false));
+  console.log(
+    "4",
+    new MyValidator(putSchema).validate({
+      value: false,
+      date: null,
+    }),
+  );
 
   return Response.json({
     value: flat?.value,
