311849ac3d33ceed80076082dc48187de37c9c95
The-LukeZ <luke.hent3005@gmail.com>
2025-07-17 23:32:02 +0200

    feat(GuildsManager): add methods to load channels and roles for the current guild

--- src/lib/stores/GuildsManager.svelte.js
+++ src/lib/stores/GuildsManager.svelte.js
@@ -9,8 +9,33 @@ export class GuildsManager {
    * @type {BotGuild[]}
    */
   guilds = $state([]);
+  /**
+   * The channels of the current guild.
+   *
+   * @type {GuildCoreChannel[] | null}
+   */
+  channels = $state([]);
+  /**
+   * The roles of the current guild.
+   *
+   * @type {GuildRole[]  | null}
+   */
+  roles = $state([]);
   currentGuild = $derived(page.params.guildid ? this.guilds.find((g) => g.id === page.params.guildid) : null);
   loaded = $state(false);
+
+  /**
+   * Whether the channels for the current guild have been loaded and are available.
+   *
+   * @type {boolean}
+   */
+  channelsLoaded = $state(false);
+  /**
+   * Whether the roles for the current guild have been loaded and are available.
+   *
+   * @type {boolean}
+   */
+  rolesLoaded = $state(false);
 
   constructor() {}
 
@@ -32,5 +57,53 @@ export class GuildsManager {
     } else {
       this.loaded = false;
     }
+  }
+
+  async loadChannels() {
+    if (!this.currentGuild) {
+      console.warn("No current guild set, cannot load channels.");
+      return;
+    }
+
+    this.channelsLoaded = false;
+
+    const channelsRes = await fetch(APIRoutes.guildChannels(this.currentGuild.id), BasicFetchInit("GET"));
+
+    if (channelsRes.ok) {
+      /** @type {GuildCoreChannel[]} */
+      let channelsJson = await channelsRes.json();
+      this.channels = channelsJson;
+      this.channelsLoaded = true;
+    } else {
+      console.error("Failed to load channels for guild:", this.currentGuild.id);
+      this.channels = [];
+      this.channelsLoaded = false;
+    }
+  }
+
+  async loadRoles() {
+    if (!this.currentGuild) {
+      console.warn("No current guild set, cannot load roles.");
+      return;
+    }
+    this.rolesLoaded = false;
+
+    const rolesRes = await fetch(APIRoutes.guildRoles(this.currentGuild.id), BasicFetchInit("GET"));
+
+    if (rolesRes.ok) {
+      /** @type {GuildRole[]} */
+      let rolesJson = await rolesRes.json();
+      this.roles = rolesJson;
+      this.rolesLoaded = true;
+    } else {
+      console.error("Failed to load roles for guild:", this.currentGuild.id);
+      this.roles = [];
+    }
+  }
+
+  clearCurrentGuild() {
+    // Since currentGuild is derived from guilds, we can't reset it directly.
+    this.channels = [];
+    this.roles = [];
   }
 }
