e87fc85b7d1e54366529068723366b5e01aed936
The-LukeZ <luke.hent3005@gmail.com>
2025-07-17 23:35:17 +0200

    feat(Mounter): add initial implementation to load channels and roles

---
+++ src/routes/g/[guildid]/Mounter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from "$app/state";
+
+  $effect(() => {
+    page.data.guildsManager.loadChannels().then(() => {
+      console.log("Channels loaded");
+    });
+
+    page.data.guildsManager.loadRoles().then(() => {
+      console.log("Roles loaded");
+    });
+  });
+</script>
