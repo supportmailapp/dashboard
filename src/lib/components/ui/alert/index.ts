8e5292a25057fbce0fed38859ebb54fb7465ceda
The-LukeZ <luke.hent3005@gmail.com>
2025-07-18 00:55:39 +0200

    see previous commit

--- src/lib/components/ui/alert/index.ts
+++ src/lib/components/ui/alert/index.ts
@@ -1,14 +1,17 @@
 import Root from "./alert.svelte";
 import Description from "./alert-description.svelte";
 import Title from "./alert-title.svelte";
+import Button from "./alert-button.svelte";
 export { alertVariants, type AlertVariant } from "./alert.svelte";
 
 export {
-	Root,
-	Description,
-	Title,
-	//
-	Root as Alert,
-	Description as AlertDescription,
-	Title as AlertTitle,
+  Root,
+  Description,
+  Title,
+  Button,
+  //
+  Root as Alert,
+  Description as AlertDescription,
+  Title as AlertTitle,
+  Button as AlertButton,
 };
