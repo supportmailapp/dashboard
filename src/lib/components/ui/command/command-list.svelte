4eea100a8af956465ac6dbac07ccdb5dc341658f
The-LukeZ <luke.hent3005@gmail.com>
2025-07-18 00:18:49 +0200

    feat(CommandList): set height to full for command list component

--- src/lib/components/ui/command/command-list.svelte
+++ src/lib/components/ui/command/command-list.svelte
@@ -8,6 +8,6 @@
 <CommandPrimitive.List
   bind:ref
   data-slot="command-list"
-  class={cn("max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto", className)}
+  class={cn("h-full max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto", className)}
   {...restProps}
 />
