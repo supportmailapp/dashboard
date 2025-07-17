bdbb489035af867181e6bc67927c547adfe4ede0
The-LukeZ <luke.hent3005@gmail.com>
2025-07-17 23:35:12 +0200

    fix: update load function to properly await parent data and log it

--- src/routes/g/[guildid]/+layout.ts
+++ src/routes/g/[guildid]/+layout.ts
@@ -1,8 +1,12 @@
 import { page } from "$app/state";
 import { isCurrentPage as _isCurrentPage, guildHref as _guildHref } from "$lib";
+import type { GuildsManager } from "$lib/stores/GuildsManager.svelte.js";
 
 export async function load({ params, parent }) {
-  await parent();
+  const data = await parent();
+  console.log("Parent Data", data);
+  // await (data.guildsManager as GuildsManager).loadChannels();
+  // await (data.guildsManager as GuildsManager).loadRoles();
   return {
     guildId: params.guildid,
     isCurrentPage: (href: string, partial: boolean = false) => _isCurrentPage(page.url, href, partial),
