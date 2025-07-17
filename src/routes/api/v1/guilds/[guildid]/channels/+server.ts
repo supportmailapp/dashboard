76ac3ab6eec897a98f0ff6e1559a1456dc56c316
The-LukeZ <luke.hent3005@gmail.com>
2025-07-17 23:32:38 +0200

    feat(GuildRoles): implement guild channels and roles endpoints

---
+++ src/routes/api/v1/guilds/[guildid]/channels/+server.ts
@@ -0,0 +1,40 @@
+import { JsonErrors } from "$lib/constants";
+import { getGuildChannels, setGuildCache } from "$lib/server/caches/guilds.js";
+
+export async function GET({ locals, url, setHeaders }) {
+  if (!locals.token || !locals.guildId) return JsonErrors.badRequest();
+
+  const bypassCache = url.searchParams.get("cache") === "false";
+
+  if (!bypassCache) {
+    const cachedChannels = getGuildChannels(locals.guildId);
+    if (cachedChannels) {
+      console.log("GET /api/v1/guilds/[guildid]/channels - Cache hit");
+      setHeaders({ "Cache-Status": "HIT" });
+      return Response.json(cachedChannels, {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  }
+
+  setHeaders({ "Cache-Status": "MISS" });
+
+  const res = await locals.discordRest.getGuildChannels(locals.guildId);
+
+  console.log("GET /api/v1/guilds/[guildid]/channels", res);
+
+  if (!res.isSuccess()) {
+    return new Response(JSON.stringify(res.error), {
+      status: res.status ?? 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  setGuildCache(locals.guildId, {
+    channels: res.data ?? [],
+  });
+
+  return Response.json(res.data, {
+    headers: { "Content-Type": "application/json" },
+  });
+}
---
+++ src/routes/api/v1/guilds/[guildid]/roles/+server.ts
@@ -0,0 +1,40 @@
+import { JsonErrors } from "$lib/constants";
+import { getGuildRoles, setGuildCache } from "$lib/server/caches/guilds.js";
+
+export async function GET({ locals, url, setHeaders }) {
+  if (!locals.token || !locals.guildId) return JsonErrors.badRequest();
+
+  const bypassCache = url.searchParams.get("cache") === "false";
+
+  if (!bypassCache) {
+    const cachedChannels = getGuildRoles(locals.guildId);
+    if (cachedChannels) {
+      console.log("GET /api/v1/guilds/[guildid]/roles - Cache hit");
+      setHeaders({ "Cache-Status": "HIT" });
+      return Response.json(cachedChannels, {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  }
+
+  setHeaders({ "Cache-Status": "MISS" });
+
+  const res = await locals.discordRest.getGuildRoles(locals.guildId);
+
+  console.log("GET /api/v1/guilds/[guildid]/roles", res);
+
+  if (!res.isSuccess()) {
+    return new Response(JSON.stringify(res.error), {
+      status: res.status ?? 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  setGuildCache(locals.guildId, {
+    roles: res.data,
+  });
+
+  return Response.json(res.data, {
+    headers: { "Content-Type": "application/json" },
+  });
+}
