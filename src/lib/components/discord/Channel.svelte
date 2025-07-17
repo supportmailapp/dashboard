661eee23ee585b9fe02329b5c14c36bd4c864be2
The-LukeZ <luke.hent3005@gmail.com>
2025-07-17 23:59:05 +0200

    feat(Channel): add Channel component for rendering channel buttons

---
+++ src/lib/components/discord/Channel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { ClassValue } from "clsx";
+  import ChannelIcon from "./ChannelIcon.svelte";
+  import { cn } from "$lib/utils";
+
+  type Props = {
+    channel?: GuildCoreChannel;
+    class?: ClassValue;
+  };
+
+  let { channel, class: className }: Props = $props();
+</script>
+
+<div
+  data-slot="channel-button"
+  class={cn(
+    "bg-blurple/60 hover:bg-blurple/80 active:bg-blurple/80 inline-flex cursor-pointer items-center gap-1 rounded-md px-2 py-1 transition duration-100",
+    className,
+  )}
+>
+  <ChannelIcon type={channel?.type} class="size-5" />
+</div>
