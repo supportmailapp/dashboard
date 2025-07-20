31ca30cd707d213c63497c2c41bb8342566e97a4
The-LukeZ <luke.hent3005@gmail.com>
2025-07-20 14:42:41 +0200

    fix: refactor TicketForumCard to improve forum configuration handling and UI clarity

--- src/routes/g/[guildid]/tickets/TicketForumCard.svelte
+++ src/routes/g/[guildid]/tickets/TicketForumCard.svelte
@@ -6,41 +6,46 @@
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import ChannelSelect from "$lib/components/ChannelSelect.svelte";
   import * as Alert from "$lib/components/ui/alert/index.js";
-  import Channel from "$lib/components/discord/Channel.svelte";
   import { Skeleton } from "$ui/skeleton";
   import { ChannelType } from "discord-api-types/v10";
   import { Button } from "$ui/button/index.js";
+  import { ConfigState } from "$lib/stores/ConfigState.svelte";
+  import Mention from "$lib/components/discord/Mention.svelte";
 
-  let { forumId }: { forumId: string | null } = $props();
+  let { forumId: fetchedForumId }: { forumId: string | null } = $props();
 
-  let forum = $state<GuildCoreChannel | null>(null);
-  let setupOpen = $state(false);
+  const forum = new ConfigState<GuildCoreChannel>(null);
   let channelsLoaded = $derived(page.data.guildsManager.channelsLoaded);
   let channels = $derived(channelsLoaded ? page.data.guildsManager.channels! : []);
   let newCategoryId = $state<string | null>(null);
 
-  async function saveFn() {}
+  async function setupFn() {}
 
   $effect(() => {
     if (channelsLoaded) {
-      forum = channels.find((channel) => channel.id === forumId) || null;
+      forum.setConfig(channels.find((channel) => channel.id === fetchedForumId) || null);
     } else {
-      forum = null;
+      forum.clear();
+    }
+  });
+
+  $effect(() => {
+    if (forum.loading) {
     }
   });
 </script>
 
-<ConfigCard title="Ticket Forum" description="Configure the forum where tickets will be created." {saveFn}>
+<ConfigCard title="Ticket Forum" description="Configure the forum where tickets will be created.">
   <div class="flex flex-col gap-2">
-    {#if channelsLoaded && forum}
+    {#if channelsLoaded && forum.isConfigured()}
       <div class="flex w-full items-center justify-between">
-        <Channel channel={forum} />
+        <Mention channel={forum.config ?? undefined} fallback="c"/>
         <Button
           variant="destructive"
           onclick={() => {
             // TODO: This is jsut a placeholder, implement actual deletion logic
-            forumId = null;
-            forum = null;
+            fetchedForumId = null;
+            forum.setConfig(null);
           }}
         >
           Reset
@@ -49,34 +54,27 @@
     {:else if !channelsLoaded}
       <Skeleton class="h-8 w-full" />
     {:else}
-      <Alert.Root variant="warning">
-        <Alert.Title>No forum set.</Alert.Title>
-        <Alert.Description>Click the button below to start the setup.</Alert.Description>
-      </Alert.Root>
-      <Dialog.Root bind:open={setupOpen}>
-        <Dialog.Trigger class={buttonVariants({ variant: "outline", class: "my-2" })}>
-          Setup Forum
-        </Dialog.Trigger>
-        <Dialog.Content class="w-80 space-y-2">
+      <div class="flex w-full flex-col">
+        <Alert.Root variant="warning" class="w-full">
+          <Alert.Title>No forum set.</Alert.Title>
+          <Alert.Description>Click the button below to start the setup.</Alert.Description>
+        </Alert.Root>
+        <div class="flex gap-1">
           <p class="text-sm">The bot needs to setup the ticket forum by itself to avoid issues.</p>
           <p class="text-sm">
             You can select a category where the forum will be created, but you can also leave it empty to let
             the bot create one for you.
           </p>
-          <Label class="mb-2">Choose a category</Label>
-          <ChannelSelect channelTypes={[ChannelType.GuildCategory]} selectCategories />
 
-          <Dialog.Footer>
-            <Dialog.Close
-              class={buttonVariants({ variant: "outline" })}
-              onclick={() => (newCategoryId = null)}
-            >
-              Cancel
-            </Dialog.Close>
-            <Button variant="default">Setup</Button>
-          </Dialog.Footer>
-        </Dialog.Content>
-      </Dialog.Root>
+          <Label class="mt-4 mb-2">Choose a category</Label>
+          <ChannelSelect
+            channelTypes={[ChannelType.GuildCategory]}
+            selectCategories
+            onSelect={(c) => (newCategoryId = c.id)}
+          />
+        </div>
+        <Button onclick={setupFn}>Setup</Button>
+      </div>
     {/if}
   </div>
 </ConfigCard>
