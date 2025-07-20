fd30c28e2eac430ae72b626638b812d7a45f8f54
The-LukeZ <luke.hent3005@gmail.com>
2025-07-20 14:07:19 +0200

    add Role and User components for displaying Discord roles and users

---
+++ src/lib/components/discord/Role.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { ClassValue } from "clsx";
+  import { cn } from "$lib/utils";
+
+  type Props = {
+    role?: GuildRole;
+    class?: ClassValue;
+  };
+
+  let { role, class: className }: Props = $props();
+  const color = $derived(role?.color ? `#${role?.color.toString(16).padStart(6, "0")}` : null);
+</script>
+
+<div data-slot="mention-container" class={cn(className)} data-id={role?.id}>
+  {#if color}
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill={color}
+        stroke="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-circle-small-icon lucide-circle-small size-5.5"
+      >
+        <circle cx="12" cy="12" r="6" />
+      </svg>
+    </div>
+  {/if}
+  <span class="text-sm font-medium">
+    {role?.name || "unknown-role"}
+  </span>
+</div>
---
+++ src/lib/components/discord/User.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { ClassValue } from "clsx";
+  import { cn } from "$lib/utils";
+  import AtSign from "@lucide/svelte/icons/at-sign";
+  import type { APIUser } from "discord-api-types/v10";
+
+  type Props = {
+    user?: Omit<APIUser, "id" | "username"> & Required<Pick<APIUser, "id" | "username">>;
+    class?: ClassValue;
+  };
+
+  let { user, class: className }: Props = $props();
+</script>
+
+<div data-slot="mention-container" class={cn(className)} data-id={user?.id}>
+  <AtSign class="size-3.5" />
+  <span class="text-sm font-medium">
+    {user?.global_name ?? user?.username ?? "unknown-user"}
+  </span>
+</div>
