0c125e28368afe783058f833e02951cc565aa2fe
The-LukeZ <luke.hent3005@gmail.com>
2025-07-20 15:44:29 +0200

    fix: adjust ChannelIcon size for improved visual consistency

--- src/lib/components/discord/Channel.svelte
+++ src/lib/components/discord/Channel.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div data-slot="mention-container" class={cn(className)} data-id={channel?.id}>
-  <ChannelIcon type={channel?.type} class="size-5" />
+  <ChannelIcon type={channel?.type} class="size-4.5" />
   <span class="text-sm font-medium">
     {channel?.name || "unknown-channel"}
   </span>
