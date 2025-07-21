54853aa5e16efa91015243fa3f8f98913088cfdd
The-LukeZ <luke.hent3005@gmail.com>
2025-07-21 22:30:17 +0200

    feat: extend SMErrorCodes enum with additional error codes for improved error handling

--- src/lib/server/constants.ts
+++ src/lib/server/constants.ts
@@ -31,3 +31,49 @@ export function ErrorJsonResponse(status: number, statusText: string, message: s
 export const ClientApiRoutes = {
   ticketSetup: () => `/ticket-setup` as const,
 };
+
+export enum SMErrorCodes {
+  UnknownError = 0,
+  GuildNotFound = 101,
+  UserNotFound = 102,
+  TimestringError = 103,
+
+  Tickets = 1000,
+  ForumNotFound = 1001,
+  CategoryCreationFailed = 1002,
+  ForumCreationFailed = 1003,
+  TicketNotFound = 1004,
+  TicketCreationFailed = 1005,
+  TicketClosed = 1006,
+  TicketsPaused = 1007,
+  TicketsDisabled = 1008,
+  TicketPostNotFound = 1009,
+  PanelNotSentOrEdited = 1010,
+  TicketSetupFailed = 1011,
+  TicketsAnonymNotEnabled = 1012,
+  CommunityRequired = 1013,
+  MissingPermissions = 1014,
+  CategoryNotFound = 1015,
+
+  Reports = 2000,
+  SendingReportFailed = 2001,
+  ReportsPaused = 2002,
+  ReportsNotEnabledOrConfigured = 2003,
+  ReportRateLimitExceeded = 2004,
+
+  AccesstokenNotFound = 3000,
+  AccesstokenInvalid = 3001,
+  NotLoggedIn = 3002,
+  CommandConfigNotFound = 3003,
+  WebhookCreationFailed = 3004,
+  /**
+   * Discord has a limit of 10 (?) webhooks per channel and 100 webhooks per guild.
+   */
+  MaxNumberOfWebhooksReached = 3005,
+  WebhookNotFound = 3006,
+
+  ClientNotFound = 5000,
+  ClientNotCreatedYet = 5001,
+  MemberNotFound = 5002,
+  DatabaseError = 3003,
+}
