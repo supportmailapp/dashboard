dd58d791dae0128481b0993e41192f1fb8a18389
The-LukeZ <luke.hent3005@gmail.com>
2025-07-22 21:21:30 +0200

    refactor: remove redirect handling logic from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,33 +18,6 @@
       }
       return;
     }
-
-    const redirectParam = page.url.searchParams.get("redirect");
-    if (redirectParam) {
-      if (redirectParam.startsWith("/g/")) {
-        // Guild-specific redirect
-        const guildId = redirectParam.split("/")[2];
-        const path = redirectParam.replace("/g/" + guildId, "");
-        localStorage.setItem("redirect", JSON.stringify({ path: path, guildId: guildId }));
-        console.log("Set redirect (1)", JSON.parse(localStorage.getItem("redirect")!));
-      } else if (redirectParam.startsWith("/")) {
-        // Redirect after selecting the guild
-        localStorage.setItem("redirect", JSON.stringify({ path: page.url.searchParams.get("redirect") }));
-        console.log("Set redirect (2)", JSON.parse(localStorage.getItem("redirect")!));
-      }
-      goto(window.location.pathname, { replaceState: true });
-    }
-
-    const redirect = JSON.parse(localStorage.getItem("redirect") || "{}") as RedirectData;
-    if (window.location.pathname == "/" && redirect.guildId) {
-      localStorage.removeItem("redirect");
-      console.log("Redirecting to", "/g/" + redirect.guildId + redirect.path);
-      goto("/g/" + redirect.guildId + redirect.path, { replaceState: true });
-    } else if (window.location.pathname.match(/^\/g\/\d+(\/\w+)?/i) && redirect.path) {
-      localStorage.removeItem("redirect");
-      console.log("Redirecting to", redirect.path);
-      goto(redirect.path, { replaceState: true });
-    }
   });
 
   beforeNavigate((nav) => {
