df4e8856e427fbe12a000ff5cb649bf490e85f0c
The-LukeZ <luke.hent3005@gmail.com>
2025-07-22 22:31:21 +0200

    refactor: reorganize mongoose imports for clarity in logEvent model

--- src/lib/server/db/models/src/logEvent.ts
+++ src/lib/server/db/models/src/logEvent.ts
@@ -1,4 +1,6 @@
-import { model, models, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
+import pkg from "mongoose";
+const { models } = pkg;
 import type { TLogEvent, LogEventType } from "supportmail-types";
 
 const logEventSchema = new Schema<TLogEvent>({
