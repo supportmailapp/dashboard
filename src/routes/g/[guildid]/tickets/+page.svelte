d2aa7d4d735bdebafcae29f025c47dcb58c3e164
The-LukeZ <luke.hent3005@gmail.com>
2025-07-23 20:48:21 +0200

    feat: implement saveAll function for ticket configuration updates

--- src/routes/g/[guildid]/tickets/+page.svelte
+++ src/routes/g/[guildid]/tickets/+page.svelte
@@ -8,10 +8,54 @@
   import { toast } from "svelte-sonner";
   import PausingCard from "./PausingCard.svelte";
   import TicketForumCard from "./TicketForumCard.svelte";
+  import apiClient from "$lib/utils/apiClient";
+  import AutoForward from "./AutoForward.svelte";
+  import type { PatchFields } from "../../../api/v1/guilds/[guildid]/config/+server";
 
   const generalTicketsConf = new ConfigState<DBGuildProjectionReturns["generalTicketSettings"]>();
 
   $inspect("generalTicketsConf", generalTicketsConf);
+
+  const saveAll: SaveFunction = async function (setLoading, callback) {
+    if (!generalTicketsConf.evalUnsaved()) {
+      toast.info("Nothing to save.");
+      return;
+    }
+
+    setLoading(true);
+
+    const current = $state.snapshot(generalTicketsConf.config);
+    try {
+      const res = await apiClient.patch(APIRoutes.ticketsConfig(page.data.guildId!), {
+        json: {
+          allowedBots: current?.allowedBots,
+          anonym: current?.anonym
+            ? {
+                enabled: current.anonym.enabled,
+                user: current.anonym.user,
+              }
+            : undefined,
+          autoForwarding: current?.autoForwarding,
+        } as PatchFields,
+      });
+
+      if (!res.ok) {
+        const error = await res.json<any>();
+        throw new Error(error.message || "Failed to save ticket configuration.");
+      }
+
+      const json = await res.json<DBGuildProjectionReturns["generalTicketSettings"]>();
+      generalTicketsConf.saveConfig(json);
+      toast.success("Ticket configuration saved successfully.");
+
+      callback?.(json);
+    } catch (error: any) {
+      toast.error(`Failed to save ticket configuration: ${error.message}`);
+      return;
+    } finally {
+      setLoading(false);
+    }
+  };
 
   $effect(() => {
     fetch(APIRoutes.ticketsConfig(page.data.guildId!))
@@ -45,6 +89,7 @@
   {#if generalTicketsConf.isConfigured()}
     <PausingCard bind:pausedUntil={generalTicketsConf.config.pausedUntil} />
     <TicketForumCard forumId={generalTicketsConf.config.forumId ?? null} wholeConfig={generalTicketsConf} />
+    <AutoForward bind:autoForward={generalTicketsConf.config.autoForwarding} saveAllFn={saveAll} />
   {:else}
     <div class="grid place-items-center">
       <LoadingSpinner />
