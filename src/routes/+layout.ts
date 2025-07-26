6ce542f58ab461c281d8f2062d86ea54446e1413
The-LukeZ <luke.hent3005@gmail.com>
2025-07-26 18:31:34 +0200

    feat: add WebSocket connection setup in layout load function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,10 @@
+import { browser } from "$app/environment";
+import { env } from "$env/dynamic/public";
+import { createWebSocketConnection } from "$lib/utils/websocket.js";
+
 export async function load({ data }) {
   return {
     user: data.user,
+    ws: data.wsToken && browser ? createWebSocketConnection(env.PUBLIC_WS_URL, data.wsToken) : undefined,
   };
 }
