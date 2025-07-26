4b913cf95dbe2c13fa4a1441190ab43efdda26f2
The-LukeZ <luke.hent3005@gmail.com>
2025-07-26 18:30:19 +0200

    feat: implement user caching with NodeCache for search results

---
+++ src/lib/server/caches/users.ts
@@ -0,0 +1,40 @@
+import { isNotUndefined } from "$lib/utils";
+import type { APIUser } from "discord-api-types/v10";
+import NodeCache from "node-cache";
+
+const users = new NodeCache({
+  stdTTL: 300,
+  errorOnMissing: false,
+  useClones: false,
+});
+
+type SearchResult = APIUser[];
+
+function buildKey(guildId: string, query: string) {
+  return `search-result:${guildId}:${encodeURIComponent(query ?? "")}`;
+}
+
+function setSearchResult(guildId: string, query: string, data: SearchResult) {
+  users.set<SearchResult>(buildKey(guildId, query), data, 60);
+}
+
+function getSearchResult(guildId: string, query: string) {
+  const _users = users.get<SearchResult>(buildKey(guildId, query));
+  return _users;
+}
+
+function takeSearchResult(guildId: string, query: string) {
+  const _users = users.take<SearchResult>(buildKey(guildId, query));
+  return _users;
+}
+
+function delSearchResult(guildId: string, query: string) {
+  return users.del(buildKey(guildId, query));
+}
+
+export default {
+  get: getSearchResult,
+  set: setSearchResult,
+  take: takeSearchResult,
+  del: delSearchResult,
+};
