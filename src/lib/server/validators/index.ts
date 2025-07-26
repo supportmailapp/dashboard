dda29a168838d6d2e3c9e17ed8ac95c1b30a3ff8
The-LukeZ <luke.hent3005@gmail.com>
2025-07-26 22:18:09 +0200

    refactor: improve validation predicates and error handling in ZodValidator

--- src/lib/server/validators/index.ts
+++ src/lib/server/validators/index.ts
@@ -1,3 +1,5 @@
+import { TextInputStyle } from "discord-api-types/v10";
+import { EntityType } from "supportmail-types";
 import { z, ZodError } from "zod";
 import { type ValidationError } from "zod-validation-error";
 import { fromZodError } from "zod-validation-error/v4";
@@ -25,6 +27,48 @@ export class ZodValidator<V extends z.ZodType> {
       };
     }
   }
+
+  static toHumanError(eror: ValidationError) {
+    const { details } = eror;
+    const errors: string[] = [];
+
+    for (let err of details) {
+      const pathLabel = err.path.length > 0 ? err.path.join(".") : "root";
+      errors.push(`${pathLabel} : ${err.message}`);
+    }
+
+    return errors.join("\n");
+  }
 }
 
 export const SnowflakePredicate = z.string().regex(/^\d{15,25}$/i);
+
+export const PartialEmojiPredicate = z
+  .object({
+    id: z.string().optional(),
+    name: z.string().optional(),
+    animated: z.boolean().optional(),
+  })
+  .refine((data) => data.id || data.name, { error: "Either id or name must be provided" });
+
+export const EntityPredicate = <T extends EntityType>(typePredicate: T) =>
+  z.object({
+    typ: z.literal(typePredicate),
+    id: SnowflakePredicate,
+  });
+
+export const UserEntityPredicate = EntityPredicate(EntityType.user);
+export const RoleEntityPredicate = EntityPredicate(EntityType.role);
+
+export const MentionableEntityPredicate = z.union([UserEntityPredicate, RoleEntityPredicate]);
+// ? Maybe add another predicate for guild?
+
+export const CustomModalFieldPredicate = z.object({
+  position: z.number().int().min(1).max(5),
+  label: z.string().min(1).max(100),
+  placeholder: z.string().min(1).max(100).optional(),
+  _required: z.boolean().default(false),
+  style: z.enum(TextInputStyle).default(TextInputStyle.Short),
+  minL: z.int().min(0).max(4000).optional(),
+  maxL: z.int().min(1).max(4000).optional(),
+});
