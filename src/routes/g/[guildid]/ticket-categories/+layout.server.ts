755c8c076c1086c013b97d37add611a9f079df11
The-LukeZ <luke.hent3005@gmail.com>
2025-07-29 21:29:13 +0200

    fix: add sorting to ticket categories retrieval for consistent order

--- src/routes/g/[guildid]/ticket-categories/+layout.server.ts
+++ src/routes/g/[guildid]/ticket-categories/+layout.server.ts
@@ -2,7 +2,7 @@ import { FlattenDocToJSON, TicketCategory } from "$lib/server/db/index.js";
 
 export async function load({ depends, locals }) {
   depends("ticket-categories");
-  const categories = await TicketCategory.find({ guildId: locals.guildId! });
+  const categories = await TicketCategory.find({ guildId: locals.guildId! }, null, { sort: { index: 1 } });
 
   return {
     categories: categories.map((doc) => FlattenDocToJSON(doc, true)),
