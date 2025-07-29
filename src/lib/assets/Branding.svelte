ac8e7e7499fe0e9d8a615f5d707a9d84632afc75
The-LukeZ <luke.hent3005@gmail.com>
2025-07-29 23:40:40 +0200

    feat: enhance branding component with dynamic text color based on mode

--- src/lib/assets/Branding.svelte
+++ src/lib/assets/Branding.svelte
@@ -1,3 +1,8 @@
+<script>
+  import { cn } from "$lib/utils";
+  import { mode } from "mode-watcher";
+</script>
+
 <a
   href="https://supportmail.dev"
   target="_blank"
@@ -5,5 +10,9 @@
   class="flex flex-row items-center justify-center gap-x-3 transition-opacity duration-120 select-none hover:opacity-75"
 >
   <img src="/logo.png" alt="Logo" class="size-14 drop-shadow-md" />
-  <span class="text-2xl font-bold text-white sm:text-3xl">SupportMail</span>
+  <span class={cn("text-2xl font-bold sm:text-3xl", mode.current === "dark" ? "text-white" : "text-black")}
+    >
+    SupportMail
+    </span
+  >
 </a>
