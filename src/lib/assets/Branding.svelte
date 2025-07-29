08c84b8c7dee72b9255aada89eeb5ee43a68b1c6
The-LukeZ <luke.hent3005@gmail.com>
2025-07-29 21:28:49 +0200

    fix: convert branding div to anchor for external link functionality

--- src/lib/assets/Branding.svelte
+++ src/lib/assets/Branding.svelte
@@ -1,4 +1,9 @@
-<div id="branding" class="flex flex-row items-center justify-center gap-x-3 select-none">
-  <img src="/logo.png" alt="Logo" class="size-16 drop-shadow-md" />
-  <span class="text-3xl font-bold text-white sm:text-4xl">SupportMail</span>
-</div>
+<a
+  href="https://supportmail.dev"
+  target="_blank"
+  id="branding"
+  class="flex flex-row items-center justify-center gap-x-3 transition-opacity duration-120 select-none hover:opacity-75"
+>
+  <img src="/logo.png" alt="Logo" class="size-14 drop-shadow-md" />
+  <span class="text-2xl font-bold text-white sm:text-3xl">SupportMail</span>
+</a>
