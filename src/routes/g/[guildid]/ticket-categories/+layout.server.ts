1d95b15b4af71cb62fcb635d63662bf7ef8fd28c
The-LukeZ <luke.hent3005@gmail.com>
2025-07-29 23:40:51 +0200

    fix: add logging for loading ticket categories by guild

--- src/routes/g/[guildid]/ticket-categories/+layout.server.ts
+++ src/routes/g/[guildid]/ticket-categories/+layout.server.ts
@@ -2,6 +2,7 @@ import { FlattenDocToJSON, TicketCategory } from "$lib/server/db/index.js";
 
 export async function load({ depends, locals }) {
   depends("ticket-categories");
+  console.log("Loading ticket categories for guild:", locals.guildId);
   const categories = await TicketCategory.find({ guildId: locals.guildId! }, null, { sort: { index: 1 } });
 
   return {
