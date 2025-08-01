b75c5334e37fd97415a571d54aa1a7b21559cabc
The-LukeZ <luke.hent3005@gmail.com>
2025-08-01 14:55:23 +0200

    feat: enhance guild caching by adding support for individual channel caching

--- src/lib/server/caches/guilds.ts
+++ src/lib/server/caches/guilds.ts
@@ -1,5 +1,6 @@
 // Caches roles and channels for guilds
 
+import type { APIThreadChannel } from "discord-api-types/v10";
 import NodeCache from "node-cache";
 
 const cache = new NodeCache({
@@ -9,9 +10,15 @@ const cache = new NodeCache({
   errorOnMissing: false,
 });
 
-const cacheKey = (guildId: string, typ: "roles" | "channels") => `${guildId}:${typ}`;
+const cacheKey = (guildId: string, typ: "roles" | "channels" | "channel", channelId = "") => {
+  if (channelId) return `${guildId}:${typ}:${channelId}` as const;
+  return `${guildId}:${typ}` as const;
+};
 
-export function setGuildCache(guildId: string, data: { roles?: GuildRole[]; channels?: GuildCoreChannel[] }) {
+export function setGuildCache(
+  guildId: string,
+  data: { roles?: GuildRole[]; channels?: GuildCoreChannel[]; channel?: GuildCoreChannel | APIThreadChannel },
+) {
   if (!guildId || !data) return;
   if (data.roles) {
     cache.set(cacheKey(guildId, "roles"), data.roles);
@@ -19,10 +26,20 @@ export function setGuildCache(guildId: string, data: { roles?: GuildRole[]; chan
   if (data.channels) {
     cache.set(cacheKey(guildId, "channels"), data.channels);
   }
+  if (data.channel) {
+    cache.set(cacheKey(guildId, "channel"), data.channel);
+  }
   if (data.roles || data.channels) {
     return true;
   }
   return false;
+}
+
+/**
+ * Used for caching specific channels in a guild, which are not in the main channels list (because maybe they are threads or posts)
+ */
+export function setChannelCache(guildId: string, channel: GuildCoreChannel | APIThreadChannel) {
+  return cache.set(cacheKey(guildId, "channel", channel.id), channel);
 }
 
 export function getGuildRoles(guildId: string): GuildRole[] | undefined {
@@ -31,6 +48,13 @@ export function getGuildRoles(guildId: string): GuildRole[] | undefined {
 
 export function getGuildChannels(guildId: string): GuildCoreChannel[] | undefined {
   return cache.get<GuildCoreChannel[]>(cacheKey(guildId, "channels"));
+}
+
+export function getGuildChannel(
+  guildId: string,
+  channelId: string,
+): GuildCoreChannel | APIThreadChannel | undefined {
+  return cache.get<GuildCoreChannel | APIThreadChannel>(cacheKey(guildId, "channel", channelId));
 }
 
 export function clearGuildCache(guildId: string) {
