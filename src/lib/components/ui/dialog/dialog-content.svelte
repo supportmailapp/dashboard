eb987b0c724739f9510ab69035ebf677b4ef8b30
The-LukeZ <luke.hent3005@gmail.com>
2025-08-01 14:54:37 +0200

    fix: conditionally render close button in dialog component

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -31,11 +31,13 @@
     {...restProps}
   >
     {@render children?.()}
-    <DialogPrimitive.Close
-      class="ring-offset-background focus:ring-ring absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
-    >
-      <XIcon />
-      <span class="sr-only">Close</span>
-    </DialogPrimitive.Close>
+    {#if withX}
+      <DialogPrimitive.Close
+        class="ring-offset-background focus:ring-ring absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+      >
+        <XIcon />
+        <span class="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    {/if}
   </DialogPrimitive.Content>
 </Dialog.Portal>
