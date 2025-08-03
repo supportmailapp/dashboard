777a61a937da14ec8bc20cfb8b71cb6cd3252dfd
The-LukeZ <luke.hent3005@gmail.com>
2025-08-03 17:16:00 +0200

    refactor: remove commented-out channel and role loading from load function

--- src/routes/g/[guildid]/+layout.ts
+++ src/routes/g/[guildid]/+layout.ts
@@ -4,8 +4,6 @@ import { isCurrentPage as _isCurrentPage, guildHref as _guildHref } from "$lib";
 export async function load({ params, parent }) {
   const data = await parent();
   console.log("Parent Data", data);
-  // await (data.guildsManager as GuildsManager).loadChannels();
-  // await (data.guildsManager as GuildsManager).loadRoles();
   return {
     guildId: params.guildid,
     isCurrentPage: (href: string, partial: boolean = false) => _isCurrentPage(page.url, href, partial),
