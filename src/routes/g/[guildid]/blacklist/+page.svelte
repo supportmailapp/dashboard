59d67eefc4d00ace9f863b0a66b12a50b2450cc7
The-LukeZ <luke.hent3005@gmail.com>
2025-08-10 04:19:39 +0200

    feat: add initial implementation of blacklist page with filter controls

---
+++ src/routes/g/[guildid]/blacklist/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import FilterControls from "./FilterControls.svelte";
+</script>
+
+<FilterControls />
+
+<!-- TODO: Fetch Button stuff -->
+
+<!-- TODO: Table View with pagination (see reports page for example) -->
