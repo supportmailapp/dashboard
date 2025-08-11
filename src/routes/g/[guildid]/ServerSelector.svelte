2a591f53ed4da0a9cde018aad88c42b5851c762e
The-LukeZ <luke.hent3005@gmail.com>
2025-08-12 00:43:26 +0200

    fix: enhance ServerSelector layout with loading spinner and card content structure

--- src/routes/g/[guildid]/ServerSelector.svelte
+++ src/routes/g/[guildid]/ServerSelector.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import LoadingSpinner from "$lib/components/LoadingSpinner.svelte";
   import * as Dialog from "$ui/dialog/index.js";
+  import { slide } from "svelte/transition";
   import ServerSelect from "../../ServerSelect.svelte";
+  import { CardContent } from "$ui/card";
 
   let {
     open = $bindable(false),
@@ -10,17 +12,22 @@
 </script>
 
 <Dialog.Root bind:open>
-  <Dialog.Content class="md:min-w-[450px]">
+  <Dialog.Content class="flex h-[40rem] flex-col gap-2 md:min-w-[450px]">
     <Dialog.Header>
       <Dialog.Title>Select a Server</Dialog.Title>
       <Dialog.Description>Choose a server from the list below.</Dialog.Description>
     </Dialog.Header>
-    {#if switchingPage}
-      <div class="grid h-100 place-items-center">
-        <LoadingSpinner class="size-20" />
-      </div>
-    {:else}
-      <ServerSelect />
-    {/if}
+    <CardContent class="flex min-h-0 flex-1 flex-col">
+      {#if switchingPage}
+        <div
+          class="grid h-full w-full place-items-center py-3"
+          transition:slide={{ duration: 200, axis: "x" }}
+        >
+          <LoadingSpinner size="20" />
+        </div>
+      {:else}
+        <ServerSelect />
+      {/if}
+    </CardContent>
   </Dialog.Content>
 </Dialog.Root>
