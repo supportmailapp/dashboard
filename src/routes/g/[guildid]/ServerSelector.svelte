c5f9e92fda438af6407dd7a649d74d42dd60e67b
The-LukeZ <luke.hent3005@gmail.com>
2025-08-11 23:09:31 +0200

    refactor: simplify ServerSelector component by removing unused site store and adjusting props

--- src/routes/g/[guildid]/ServerSelector.svelte
+++ src/routes/g/[guildid]/ServerSelector.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
   import LoadingSpinner from "$lib/components/LoadingSpinner.svelte";
-  import { site } from "$lib/stores/site.svelte";
   import * as Dialog from "$ui/dialog/index.js";
   import ServerSelect from "../../ServerSelect.svelte";
 
   let {
     open = $bindable(false),
     switchingPage = $bindable(false),
-  }: { open: boolean; switchingPage?: boolean } = $props();
+  }: { open?: boolean; switchingPage?: boolean } = $props();
 </script>
 
-<Dialog.Root bind:open>
-  <!-- <Dialog.Overlay class="bg-overlay fixed inset-0 z-50" /> -->
+<Dialog.Root {open}>
   <Dialog.Content class="md:min-w-[450px]">
     <Dialog.Header>
       <Dialog.Title>Select a Server</Dialog.Title>
       <Dialog.Description>Choose a server from the list below.</Dialog.Description>
     </Dialog.Header>
-    {#if site.showLoading}
+    {#if switchingPage}
       <div class="grid h-100 place-items-center">
         <LoadingSpinner class="size-20" />
       </div>
     {:else}
-      <ServerSelect clickFn={() => (switchingPage = true)} />
+      <ServerSelect />
     {/if}
   </Dialog.Content>
 </Dialog.Root>
