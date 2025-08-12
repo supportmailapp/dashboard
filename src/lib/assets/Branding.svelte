2818fe8d4d3fdfbc0ac14b755fc6ead6a8141b44
The-LukeZ <luke.hent3005@gmail.com>
2025-08-12 23:02:40 +0200

    fix: simplify Branding component by removing unused script and updating text color logic

--- src/lib/assets/Branding.svelte
+++ src/lib/assets/Branding.svelte
@@ -1,8 +1,3 @@
-<script>
-  import { cn } from "$lib/utils";
-  import { mode } from "mode-watcher";
-</script>
-
 <a
   href="https://supportmail.dev"
   target="_blank"
@@ -10,12 +5,7 @@
   class="flex flex-row items-center justify-center gap-x-3 transition-opacity duration-120 select-none hover:opacity-75"
 >
   <img src="/logo.png" alt="Logo" class="size-14 drop-shadow-md" />
-  <span
-    class={cn(
-      "text-2xl font-bold text-shadow-black/20 text-shadow-md sm:text-3xl",
-      mode.current !== "dark" ? "text-black" : "text-white",
-    )}
-  >
+  <span class="text-2xl font-bold text-foreground text-shadow-accent/30 text-shadow-md sm:text-3xl">
     SupportMail
   </span>
 </a>
