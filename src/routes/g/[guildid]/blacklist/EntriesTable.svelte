bf23a70893db3107f1ff802d49b081e4868d5c83
The-LukeZ <luke.hent3005@gmail.com>
2025-08-14 01:35:47 +0200

    feat: add EntriesTable component for displaying blacklist entries

---
+++ src/routes/g/[guildid]/blacklist/EntriesTable.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { IBlacklistEntry } from "supportmail-types";
+  import { type ColumnDef } from "@tanstack/table-core";
+  import DataTable from "$lib/components/data-table.svelte";
+
+  let { entries }: { entries: DocumentWithId<IBlacklistEntry>[] } = $props();
+
+  const cols: ColumnDef<IBlacklistEntry>[] = [
+    {
+      accessorKey: "scope",
+      header: "Scope",
+    },
+    {
+      accessorKey: "type",
+      header: "Type",
+    },
+    {
+      accessorKey: "id",
+      header: "Entry",
+    },
+  ];
+</script>
+
+<DataTable columns={cols} data={entries} />
