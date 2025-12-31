2c06a6875b2453510c1c407233d01d346f260e9b
The-LukeZ <contact.lukez@proton.me>
2025-12-31 17:38:21 +0100

    feat: add Unsplash namespace with image and user types for API integration

--- src/app.d.ts
+++ src/app.d.ts
@@ -246,6 +246,132 @@ declare global {
       }>;
     } & (POSTTicketSetupJSONResultSuccess | POSTTicketSetupJSONResultError);
   }
+
+  namespace Unsplash {
+    /**
+     * The simplified image object returned by our API and used in the frontend.
+     */
+    type MyImage = {
+      /**
+       * The ID of the image.
+       */
+      id: string;
+      /**
+       * The URL of the image.
+       */
+      url: string;
+      /**
+       * The url to the image page on Unsplash.
+       */
+      htmlUrl: string;
+      /**
+       * The author of the image.
+       */
+      authorName: string;
+      /**
+       * The author's Unsplash profile URL.
+       */
+      authorProfileUrl: string;
+      /**
+       * The blur hash of the image.
+       */
+      blurHash: string;
+    };
+
+    type StoredImage = {
+      /** ISO 8601 string representing the timestamp when the image was stored. (expires after 5 minutes) */
+      timestamp: string;
+      image: MyImage;
+    };
+
+    /**
+     * Represents an image object returned by the Unsplash API.
+     */
+    type Image = {
+      id: string;
+      created_at: string;
+      updated_at: string;
+      width: number;
+      height: number;
+      color: string;
+      blur_hash: string;
+      downloads: number;
+      description: string | null;
+      exif: Unsplash.ExifData | null;
+      location: {
+        name: string | null;
+        city: string | null;
+        country: string | null;
+        position: {
+          latitude: number;
+          longitude: number;
+        } | null;
+      } | null;
+      current_user_collections: Array<Unsplash.UserCollection>;
+      urls: {
+        raw: string;
+        full: string;
+        regular: string;
+        small: string;
+        thumb: string;
+      };
+      links: {
+        self: string;
+        html: string;
+        download: string;
+        download_location: string;
+      };
+      user: Unsplash.User;
+    };
+
+    type User = {
+      id: string;
+      updated_at: string;
+      username: string;
+      name: string;
+      portfolio_url: string | null;
+      bio: string;
+      location: string;
+      total_collections: number;
+      instagram_username: string | null;
+      twitter_username: string | null;
+      links: {
+        self: string;
+        html: string;
+        photos: string;
+        portfolio: string;
+      };
+    };
+
+    type UserCollection = {
+      id: number;
+      title: string;
+      published_at: string;
+      last_collected_at: string;
+      updated_at: string;
+      cover_photo: unknown;
+      user: unknown; // can be null? wtf Unsplash docs
+    };
+
+    type ExifData = {
+      make: string | null;
+      model: string | null;
+      exposure_time: string | null;
+      aperture: string | null;
+      focal_length: string | null;
+      iso: number | null;
+    };
+
+    type GetImageRes =
+      | {
+          type: "error";
+          message: string;
+        }
+      | {
+          type: "success";
+          image: Unsplash.MyImage;
+        };
+  }
 }
 
 export {};
