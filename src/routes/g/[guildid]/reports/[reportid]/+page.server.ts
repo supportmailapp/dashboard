64bd44c9b762fc8c2871b492b79ff882a10fed14
The-LukeZ <contact.lukez@proton.me>
2026-01-01 16:54:14 +0100

    fix: add validation for report ID and improve error handling for report retrieval

--- src/routes/g/[guildid]/reports/[reportid]/+page.server.ts
+++ src/routes/g/[guildid]/reports/[reportid]/+page.server.ts
@@ -1,8 +1,17 @@
 import { FlattenDocToJSON, Report, type FlattenDocResult } from "$lib/server/db";
 import type { IReport } from "$lib/sm-types";
+import { error } from "@sveltejs/kit";
+import { isValidObjectId } from "mongoose";
 
 export async function load({ params }) {
   const { guildid, reportid } = params;
+
+  if (!isValidObjectId(reportid)) {
+    error(400, {
+      message: "Invalid report ID",
+      status: 400,
+    });
+  }
 
   const report = await Report.findOne({
     guildId: guildid,
@@ -13,10 +22,14 @@ export async function load({ params }) {
   console.log("report", report);
 
   if (!report) {
-    return {
-      report: null,
-      error: "Report not found",
-    };
+    // return {
+    //   report: null,
+    //   error: "Report not found",
+    // };
+    error(404, {
+      message: "Report not found",
+      status: 404,
+    });
   }
 
   const sanitizedReport: FlattenDocResult<IReport, true> = FlattenDocToJSON(report, true);
