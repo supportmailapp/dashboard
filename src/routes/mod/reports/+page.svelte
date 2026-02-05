e9e122ebeabccebe11af4b4b35a5b9fbd7aba991
The-LukeZ <contact.lukez@proton.me>
2026-02-05 15:53:54 +0100

    feat: add reports page with breadcrumb management

---
+++ src/routes/mod/reports/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import { getBreadcrumbs } from "../breadcrumb.svelte";
+
+  const crumbs = getBreadcrumbs();
+  onMount(() => {
+    crumbs.push("Reports");
+  });
+  onDestroy(() => {
+    crumbs.pop();
+  });
+</script>
+
+You are a mod, do mod stuff here.
